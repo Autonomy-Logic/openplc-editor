f924313fac8068acd43a1de5a8c97842f9da69af
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-11-06 14:40:51 -0300

    fix structure of dataTypeSchema

--- src/types/common/dataType.ts
+++ src/types/common/dataType.ts
@@ -1,15 +1,68 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const dataTypeSchema = z.object({
-    dataType: z.array(z.object({
-        '@name':z.string(),
-        baseType:z.object({
-            type: z.enum(['BOOL', 'INT', 'DINT']),
+  '@name': z.string(),
+  baseType: z.array(
+    z.object({
+      derivationType: z.object({
+        directly: z.object({
+          type: z.enum(['BOOL', 'INT', 'DINT']),
         }),
-        initialValue: z.object({
-            simpleValue: z.object({
-                '@value': z.string(),
+        subrange: z.object({
+          subrangeUnsigned: z.object({
+            range: z.array(
+              z.object({
+                '@lower': z.string().default('0'),
+                '@upper': z.string().default('0'),
+                baseType: z.object({
+                  type: z.enum(['BOOL', 'INT', 'DINT']),
+                }),
+              }),
+            ),
+          }),
+        }),
+        enumerated: z.object({
+          enum: z.object({
+            values: z.object({
+              value: z.object({
+                '@name': z.string(),
+              }),
             }),
+          }),
         }),
-    }))
-})
+        array: z.object({
+          array: z.object({
+            dimension: z.array(
+              z.object({
+                '@lower': z.string(),
+                '@upper': z.string(),
+                baseType: z.object({
+                  derived: z.object({
+                    '@name': z.string(),
+                  }),
+                }),
+              }),
+            ),
+          }),
+        }),
+        struct: z.object({
+          struct: z.object({
+            variable: z.array(
+              z.object({
+                '@name': z.string(),
+                type: z.enum(['BOOL', 'INT', 'DINT']),
+              }),
+            ),
+          }),
+        }),
+      }),
+    }),
+  ),
+  initialValue: z
+    .object({
+      simpleValue: z.any(),
+    })
+    .optional(),
+});
+
+export type DataTypeShape = z.infer<typeof dataTypeSchema>;
