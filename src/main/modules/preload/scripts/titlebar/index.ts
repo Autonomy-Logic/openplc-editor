d509bcfb15eb7ddb305cc490a58612889226f492
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-12-06 07:59:52 -0300

    💄style: change the titlebar color

--- src/main/modules/preload/scripts/titlebar/index.ts
+++ src/main/modules/preload/scripts/titlebar/index.ts
@@ -1,11 +1,11 @@
 import { Titlebar,TitlebarColor } from 'custom-electron-titlebar';
-import colors from 'tailwindcss/colors';
+// import colors from 'tailwindcss/colors';
 
 // Wip: Should export the titlebar instance
 window.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line no-new
   new Titlebar({
     containerOverflow: 'hidden',
-    backgroundColor: TitlebarColor?.fromHex(colors.gray['900']),
+    backgroundColor: TitlebarColor?.fromHex("#121316"),
   });
 });
