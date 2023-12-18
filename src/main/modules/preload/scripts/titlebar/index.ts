025ccd804912fbcf4bfa8f13220e1473a831ad4e
MarcosGAC <marcosgabriel.dev@gmail.com>
2023-12-18 16:23:24 -0300

    🚩feat: titlebar icon

--- src/main/modules/preload/scripts/titlebar/index.ts
+++ src/main/modules/preload/scripts/titlebar/index.ts
@@ -1,11 +1,29 @@
-import { Titlebar,TitlebarColor } from 'custom-electron-titlebar';
+import { Titlebar, TitlebarColor } from 'custom-electron-titlebar';
 // import colors from 'tailwindcss/colors';
-
+import logoIcon from './assets/logoIcon'
 // Wip: Should export the titlebar instance
 window.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line no-new
-  new Titlebar({
+  const options = {
     containerOverflow: 'hidden',
-    backgroundColor: TitlebarColor?.fromHex("#121316"),
-  });
+    backgroundColor: TitlebarColor?.fromHex('#121316'),
+    icon: logoIcon,
+  };
+  // eslint-disable-next-line no-new
+  new Titlebar(options);
 });
+
+//
+
+// const [menubar] = document.getElementsByClassName('cet-menubar');
+
+// if (menubar) {
+//   const menuButtonDiv = document.createElement('div');
+//   menuButtonDiv.className = 'cet-menubar-logo';
+//   const logo = document.createElement('img');
+//   logo.src = OpenPLCIcon;
+//   logo.alt = 'OpenPLC logo';
+
+//   menuButtonDiv.appendChild(logo);
+
+//   menubar.appendChild(menuButtonDiv);
+// }
--- src/renderer/contexts/Tabs.tsx
+++ src/renderer/contexts/Tabs.tsx
@@ -4,21 +4,14 @@ import {
   FC,
   PropsWithChildren,
   useCallback,
-  useEffect,
   useMemo,
   useState,
 } from 'react';
-import { createRoot, Root } from 'react-dom/client';
-import { useNavigate } from 'react-router-dom';
 
-import { CONSTANTS } from '@/utils';
 
-import { TitlebarTabs } from '../components';
-import OpenPLCIcon from '../assets/icons/openPLCIcon.svg';
 /**
  * Extracted 'path' property from the imported CONSTANTS object.
  */
-const { paths } = CONSTANTS;
 /**
  * Type definition for individual tab properties.
  */
@@ -74,18 +67,8 @@ export const TabsContext = createContext<TabsContextData>({} as TabsContextData)
 // Review this eslint rule
 // eslint-disable-next-line react/function-component-definition
 const TabsProvider: FC<PropsWithChildren> = ({ children }) => {
-  /**
-   * State to hold the root element for titlebar tabs.
-   */
-  const [rootTitlebarTabs, setRootTitlebarTabs] = useState<Root>();
-  /**
-   * State to store the list of tabs.
-   */
   const [tabs, setTabs] = useState<TabsProps[]>([]);
-  /**
-   * Hook to navigate between routes.
-   */
-  const navigate = useNavigate();
+
   /**
    * Adds a new tab to the list of tabs.
    * @param tab - The tab properties to add.
@@ -117,73 +100,6 @@ const TabsProvider: FC<PropsWithChildren> = ({ children }) => {
     (id: number | string) => setTabs((state) => [...state.filter((tab) => tab.id !== id)]),
     []
   );
-  /**
-   * Sets the currently active tab.
-   * @param id - The ID of the tab to set as active.
-   */
-  const setCurrentTab = useCallback(
-    (id: number | string) =>
-      setTabs((state) =>
-        state.map((tab) => (tab.id === id ? { ...tab, current: true } : { ...tab, current: false }))
-      ),
-    []
-  );
-  /**
-   * Gets the currently active tab.
-   */
-  const getCurrentTab = useCallback(() => tabs.find(({ current }) => current), [tabs]);
-  /**
-   * Use effect hook to create and render the titlebar tabs.
-   */
-  useEffect(() => {
-    const [menubar] = document.getElementsByClassName('cet-menubar');
-    const titlebarTabs = document.createElement('div');
-
-    titlebarTabs.className = 'flex flex-1 items-center overflow-hidden';
-    titlebarTabs.id = 'titlebar-windows';
-  
-    if (menubar) {
-    const menuButtonDiv = document.createElement('div');
-    menuButtonDiv.className = 'cet-menubar-logo';
-    const logo = document.createElement('img');
-    logo.src = OpenPLCIcon; 
-    logo.alt = 'OpenPLC logo'; 
-
-
-
-    menuButtonDiv.appendChild(logo);
-
-    menubar.appendChild(menuButtonDiv);
-
-      menubar.after(titlebarTabs);
-      const root = createRoot(titlebarTabs as HTMLElement);
-      root.render(<TitlebarTabs tabs={[]} />);
-      setRootTitlebarTabs(root);
-    }
-  }, []);
-  /**
-   * Use effect hook to update the titlebar tabs when tabs change.
-   */
-  useEffect(() => {
-    if (rootTitlebarTabs) {
-      rootTitlebarTabs.render(
-        <TitlebarTabs
-          tabs={tabs.map((tab) => ({
-            ...tab,
-            onClick: () => {
-              tab.onClick && tab.onClick();
-              setCurrentTab(tab.id);
-            },
-            onClickCloseButton: () => {
-              tab.onClickCloseButton && tab.onClickCloseButton();
-              removeTab(tab.id);
-              getCurrentTab()?.id === tab.id && navigate(paths.MAIN);
-            },
-          }))}
-        />
-      );
-    }
-  }, [getCurrentTab, navigate, removeTab, rootTitlebarTabs, setCurrentTab, tabs]);
   /**
    * Memoize the tabs context data.
    */
