340b15409b09b8594787dd030abce61e83b48045
João GS Pereira <joaogspe@outlook.com>
2023-12-26 11:08:14 -0300

    :sparkles: feat: create container to display the example cards

--- src/renderer/pages/draft.tsx
+++ src/renderer/pages/draft.tsx
@@ -1,10 +1,38 @@
-import RecentProjects from '../../shared/data/mock/projects-data.json';
-import RecentProjectViewer from '../components/recent-project';
+import LeftArrow from 'renderer/assets/icons/left-arrow-muted.svg';
+import RightArrow from 'renderer/assets/icons/right-arrow.svg';
+
+import DataForExamples from '../../shared/data/mock/examples.json';
+import TestImage from '../assets/images/example.png';
+import Card from '../components/elements/card';
 
 function Draft() {
   return (
     <div className='w-full h-full flex justify-center items-center'>
-      <RecentProjectViewer dataToRender={RecentProjects} />
+      <div className='relative flex flex-col items-start justify-start gap-6 text-left text-xl text-black w-full bg-white'>
+        <div className='flex flex-row items-start self-stretch justify-between'>
+          <h3 className='relative leading-6 font-medium'>Examples</h3>
+          <div className='flex flex-row flex-shrink-0 items-start justify-start gap-6'>
+            <img
+              alt='icon for nav left'
+              className='relative flex-shrink-0 w-6 h-6 overflow-hidden object-cover'
+              src={LeftArrow}
+            />
+            <img
+              alt='icon for nav right'
+              className='relative flex-shrink-0 w-6 h-6 overflow-hidden object-contain'
+              src={RightArrow}
+            />
+          </div>
+        </div>
+        <div className='flex flex-row self-stretch items-start justify-start gap-6 overflow-auto'>
+          {DataForExamples.map((ex) => (
+            <Card.Root key={ex.example_id}>
+              <Card.Preview source={TestImage} />
+              <Card.Label title={ex.example_name} description={ex.example_description} />
+            </Card.Root>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
