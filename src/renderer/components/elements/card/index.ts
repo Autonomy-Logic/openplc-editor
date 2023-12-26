8efa0e439f14b3955f5d6f8da22402ceb7520b88
João GS Pereira <joaogspe@outlook.com>
2023-12-26 09:11:32 -0300

    :sparkles: feat: create card component

---
+++ src/renderer/components/elements/card/index.ts
@@ -0,0 +1,11 @@
+import Label from './card-label';
+import Preview from './card-preview';
+import Root from './card-root';
+
+const Card = {
+  Label,
+  Preview,
+  Root,
+};
+
+export default Card;
