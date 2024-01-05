402ff7c7f2857f063bb218880eaf0316b8eb476d
Joao GS Pereira <joaogspe@outlook.com>
2024-01-05 09:45:40 -0300

    :coffin: fix: remove dead code
    remove dead code that was blocking the build process

--- src/renderer/features/screens/start/components/layout/index.tsx
+++ src/renderer/features/screens/start/components/layout/index.tsx
@@ -3,5 +3,5 @@ import { HTMLAttributes, ReactNode } from 'react';
 export type StartScreenLayoutProps = HTMLAttributes<HTMLDivElement>;
 
 export default function Layout(props: StartScreenLayoutProps): ReactNode {
-  return <main className='w-full h-screen flex pl-[92px] pr-[70px]' {...props} />;
+  return <main className='w-full h-screen flex pl-[92px] pr-[70px] py-4' {...props} />;
 }
