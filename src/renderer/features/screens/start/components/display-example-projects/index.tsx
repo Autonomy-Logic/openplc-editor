f5cc2f936e47e314e2846922691f540d67da2abe
Joao GS Pereira <joaogspe@outlook.com>
2024-01-16 18:03:30 -0300

    :bug: fix: fix carousel alignment

--- src/renderer/features/screens/start/components/display-example-projects/index.tsx
+++ src/renderer/features/screens/start/components/display-example-projects/index.tsx
@@ -1,42 +1,47 @@
-import useEmblaCarousel from 'embla-carousel-react';
-import { useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react'
+import { useCallback } from 'react'
 // TODO: Remove mock data
-import MockImage from '~renderer/assets/images/example.png';
+import MockImage from '~renderer/assets/images/example.png'
 
-import Card from '~renderer/components/elements/card';
+import Card from '~renderer/components/elements/card'
 
 // TODO: Remove mock data
-import Mock from '../../../../../../shared/data/mock/examples.json';
-import { Carousel, Header, NextButton, PrevButton, Wrapper } from './elements';
+import Mock from '../../../../../../shared/data/mock/examples.json'
+import { Carousel, Header, NextButton, PrevButton, Wrapper } from './elements'
 
 export default function DisplayExampleProjects() {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+	const [emblaRef, emblaApi] = useEmblaCarousel({
+		align: 'start',
+	})
 
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev();
-  }, [emblaApi]);
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev()
+	}, [emblaApi])
 
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext();
-  }, [emblaApi]);
-  return (
-    <Wrapper>
-      <Header title='Examples'>
-        <PrevButton onClick={scrollPrev} />
-        <NextButton onClick={scrollNext} />
-      </Header>
-      <Carousel.Container refProvider={emblaRef}>
-        <Carousel.Viewport>
-          {Mock.map((project) => (
-            <Card.Root key={project.id}>
-              <Card.Preview source={MockImage} />
-              <Card.Label title={project.name} description={project.description} />
-            </Card.Root>
-          ))}
-        </Carousel.Viewport>
-      </Carousel.Container>
-    </Wrapper>
-  );
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext()
+	}, [emblaApi])
+	return (
+		<Wrapper>
+			<Header title='Examples'>
+				<PrevButton onClick={scrollPrev} />
+				<NextButton onClick={scrollNext} />
+			</Header>
+			<Carousel.Container refProvider={emblaRef}>
+				<Carousel.Viewport>
+					{Mock.map((project) => (
+						<Card.Root key={project.id}>
+							<Card.Preview source={MockImage} />
+							<Card.Label
+								title={project.name}
+								description={project.description}
+							/>
+						</Card.Root>
+					))}
+				</Carousel.Viewport>
+			</Carousel.Container>
+		</Wrapper>
+	)
 }
 
-export type DisplayExampleProjectsComponent = typeof DisplayExampleProjects;
+export type DisplayExampleProjectsComponent = typeof DisplayExampleProjects
