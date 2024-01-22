521dd0997e89c4f3b3f7d6112472ba827ba90240
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-01-22 11:12:20 -0300

    fix: search size

--- src/renderer/features/screens/start/components/actions-bar/index.tsx
+++ src/renderer/features/screens/start/components/actions-bar/index.tsx
@@ -45,7 +45,7 @@ export default function ActionsBar() {
 	}, [])
 
 	return (
-		<div className='w-full  pr-10 xl:pr-7 xxl:pr-10 flex mb-4 items-center justify-between relative'>
+		<div className='w-full  pr-10 xl:pr-7 xxl:pr-10 flex mb-4 items-center gap-4 justify-between relative'>
 			<Dropdown.Root ref={dropdownRef}>
 				<Dropdown.Select
 					className='w-60 h-full gap-3 whitespace-nowrap text-base font-caption text-neutral-700 font-normal justify-center pl-6 pr-2 relative flex items-center rounded-lg  border-neutral-100 border bg-white dark:bg-neutral-50 '
@@ -65,7 +65,7 @@ export default function ActionsBar() {
 					selectedOption={selectedOption}
 				/>
 			</Dropdown.Root>
-			<Search.Root className='flex w-[704px] border-neutral-100 border-[1.5px] gap-4 px-8 rounded-lg text-base font-caption items-center h-14 bg-white dark:bg-neutral-50'>
+			<Search.Root className='flex flex-grow w-[704px] border-neutral-100 border-[1.5px] gap-4 px-8 rounded-lg text-base font-caption items-center h-14 bg-white dark:bg-neutral-50'>
 				<StartSearchIcon />
 				<Search.Input
 					className='w-full h-full bg-inherit outline-none text-black'
--- src/renderer/features/screens/start/components/container/index.tsx
+++ src/renderer/features/screens/start/components/container/index.tsx
@@ -4,5 +4,5 @@ export type StartScreenLayoutProps = HTMLAttributes<HTMLDivElement>;
 
 export default function Container(props: StartScreenLayoutProps): ReactNode {
   //pl-[92px] pr-[70px]
-  return <main className="w-full h-screen flex pl-16 py-4 pr-14 " {...props} />;
+  return <main className="w-full h-screen flex  py-4 pl-[7%] pr-[5%]" {...props} />;
 }
