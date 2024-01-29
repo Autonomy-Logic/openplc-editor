e661d6e729dba14d227cd22fe1073ef87ce72ba6
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-01-29 13:04:00 -0300

    ✅test: create test for carousel component

---
+++ src/renderer/features/screens/start/components/display-example-projects/elements/carousel/carousel.spec.tsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import Carousel from "./index";
+import "@testing-library/jest-dom";
+
+describe("Viewport Component", () => {
+  it("renders without crashing", () => {
+    const { container } = render(<Carousel.Viewport />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("renders with additional class name", () => {
+    const { container } = render(
+      <Carousel.Viewport className="custom-class" />,
+    );
+    expect(container.firstChild).toHaveClass("custom-class");
+  });
+});
+
+describe("Container Component", () => {
+  it("renders without crashing", () => {
+    const { container } = render(<Carousel.Container refProvider={() => {}} />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it("renders with additional class name", () => {
+    const { container } = render(
+      <Carousel.Container className="custom-class" refProvider={() => {}} />,
+    );
+    expect(container.firstChild).toHaveClass(
+      "custom-class",
+    );
+  });
+});
