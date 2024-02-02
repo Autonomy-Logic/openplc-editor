62c3a23e9452bd94fca874eff8d17a72ed087ee0
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-02-02 12:02:48 -0300

    fix: fix spacing of menu

--- src/renderer/features/screens/start/components/display-recent-projects/elements/wrapper/index.tsx
+++ src/renderer/features/screens/start/components/display-recent-projects/elements/wrapper/index.tsx
@@ -4,7 +4,7 @@ export type RecentWrapperProps = HTMLAttributes<HTMLDivElement>
 
 export default function Wrapper({ children }: RecentWrapperProps): ReactNode {
 	return (
-		<section className='flex flex-col w-full h-1/2 2xl:h-3/5 3xl:h-3/4 4xl:h-4/5 pr-9 4xl:pr-0'>
+		<section className='flex flex-col w-full h-[52%] 2xl:h-3/5 3xl:h-3/4 4xl:h-4/5 pr-9 4xl:pr-0'>
 			{children}
 		</section>
 	)
