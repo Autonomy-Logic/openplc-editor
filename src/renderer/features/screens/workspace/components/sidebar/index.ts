4389110185ae7e6130d7ee325e623e01e6cc31c7
Joao GS Pereira <joaogspe@outlook.com>
2024-02-09 11:04:11 -0300

    :sparkles: WIP/feat: create the project tree base component

--- src/renderer/features/screens/workspace/components/sidebar/index.ts
+++ src/renderer/features/screens/workspace/components/sidebar/index.ts
@@ -1,5 +1,7 @@
 import { Wrapper } from './wrapper'
+import { ProjectTree } from './project-tree'
 
 export const SidebarComponent = {
 	Wrapper,
+	ProjectTree
 }
---
+++ src/renderer/features/screens/workspace/components/sidebar/project-tree/index.tsx
@@ -0,0 +1,7 @@
+export const ProjectTree = () => {
+	return (
+		<div className='w-full h-1/2 bg-neutral-50 dark:bg-neutral-950 border-none'>
+			Project Tree
+		</div>
+	)
+}
