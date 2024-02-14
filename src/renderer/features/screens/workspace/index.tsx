c6d2f1dec3f3e546f593e4455565526b33df1630
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-02-14 11:05:50 -0300

    feat: create resizable effect

--- src/renderer/features/screens/workspace/index.tsx
+++ src/renderer/features/screens/workspace/index.tsx
@@ -1,32 +1,59 @@
-import { useLocation } from "react-router-dom";
-import { useOpenPLCStore } from "~/renderer/store";
+import React, { useEffect, useState } from "react";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 export default function Workspace() {
-  // const location = useLocation();
-  // console.log(location.pathname);
+  const [sidebarSize, setSidebarSize] = useState(11);
+  const [bottonbarSize, setBottonbarSize] = useState(22);
 
-  // const { projectDataDraft, projectPathDraft } = useOpenPLCStore();
+  useEffect(() => {
+    setSidebarSize(prevSize => (prevSize < 11 ? 10 : prevSize));
+  }, []);
+
+  useEffect(() => {
+    setBottonbarSize(prevSize => (prevSize < 22 ? 21 : prevSize));
+  }, []);
 
   return (
     <div className="containerWrapper bg-[#011E4B] flex h-full items-center w-full">
       <div className="activitybar h-full w-20 bg-[#011E4B]"></div>
-      <div className="rounded-tl-lg flex-grow h-full bg-neutral-300 flex gap-2 p-2">
-        <div className="sidebar h-full w-[200px] border-inherit rounded-lg overflow-hidden border-ne">
-          <div className="projects h-[40%]  border-neutral-200 bg-white"></div>
-          <hr className="h-[1px] bg-neutral-600 w-full" />
-          <div className="h-[60%] border-neutral-200 bg-white"></div>
+      <PanelGroup direction="horizontal">
+        <div className="rounded-tl-lg flex-grow h-full bg-neutral-300 flex p-2">
+          <Panel
+            onResize={setSidebarSize}
+            id="sidebar"
+            minSize={10}
+            defaultSize={sidebarSize}
+            className={`sidebar h-full border-inherit rounded-lg overflow-hidden ${sidebarSize < 11 ? "hidden" : ""}`}
+          >
+            <div className="projects h-[40%] border-neutral-200 bg-white"></div>
+            <hr className="h-[1px] bg-neutral-600 w-full" />
+            <div className="h-[60%] border-neutral-200 bg-white"></div>
+          </Panel>
+          <PanelResizeHandle className="hover:bg-neutral-400 w-2" />
+          <Panel>
+            <PanelGroup
+              className="flex-grow h-full overflow-hidden flex flex-col gap-1"
+              direction="vertical"
+            >
+              <div className="w-full h-[70px] border-neutral-200 rounded-lg mb-1 bg-white"></div>
+              <Panel className="flex-grow rounded-lg overflow-hidden flex flex-col">
+                <div className="w-full flex flex-col flex-grow">
+                  <div className="w-full h-[208px] border-neutral-200 bg-white"></div>
+                  <div className="flex-grow w-full border-neutral-200 bg-white"></div>
+                </div>
+              </Panel>
+              <PanelResizeHandle />
+              <Panel
+                onResize={setBottonbarSize}
+                id="bottonbar"
+                minSize={21}
+                defaultSize={bottonbarSize}
+                className={`border-neutral-200 bg-white rounded-lg ${bottonbarSize < 22 ? "hidden" : ""}`}
+              ></Panel>
+            </PanelGroup>
+          </Panel>
         </div>
-        <div className="flex-grow h-full overflow-hidden flex flex-col gap-2">
-          <div className="w-full h-[70px] border-neutral-200 rounded-lg bg-white"></div>
-          <div className="flex-grow rounded-lg overflow-hidden flex flex-col ">
-            <div className="w-full flex flex-col flex-grow">
-              <div className="w-full h-[208px] border-neutral-200 bg-white"></div>
-              <div className=" flex-grow w-full border-neutral-200 bg-white"></div>
-            </div>
-          </div>
-          <div className="h-60 border-neutral-200 bg-white rounded-lg"></div>
-        </div>
-      </div>
+      </PanelGroup>
     </div>
   );
 }
