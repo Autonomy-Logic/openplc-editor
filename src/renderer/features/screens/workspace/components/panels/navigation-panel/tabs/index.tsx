add2ad2a0154d8b52f6620eb99617c75f06d2383
JoaoGSP <joaogspe@outlook.com>
2024-02-19 14:13:06 -0300

    :sparkles: feat: create the tabs component

--- src/renderer/features/screens/workspace/components/panels/navigation-panel/tabs/index.tsx
+++ src/renderer/features/screens/workspace/components/panels/navigation-panel/tabs/index.tsx
@@ -1,11 +1,12 @@
-import { PlusIcon } from '~/renderer/assets'
+import { useRef, useState } from 'react'
+import { LDIcon, PlusIcon } from '~/renderer/assets'
 import { cn } from '~/utils'
 
 const tabs = [
-	{ name: 'My Account', href: '#', current: true },
-	{ name: 'Company', href: '#', current: false },
-	{ name: 'Team Members', href: '#', current: false },
-	{ name: 'Billing', href: '#', current: false },
+	{ name: 'Program', href: '#', current: true },
+	{ name: 'Function', href: '#', current: false },
+	{ name: 'Function Block', href: '#', current: false },
+	{ name: 'Data Type', href: '#', current: false },
 ]
 export const NavigationPanelTabs = () => {
 	return (
@@ -17,14 +18,15 @@ export const NavigationPanelTabs = () => {
 					className={cn(
 						tab.current
 							? 'border-b-[3px] border-brand'
-							: 'opacity-[35%] border-r-2 border-neutral-300',
+							: 'opacity-[35%] border-r-2 border-neutral-300 dark:bg-neutral-900 dark:border-neutral-800',
 						tabIdx === 0 ? 'rounded-tl-lg' : '',
-						'group relative min-w-0 max-w-[160px] bg-neutral-100 h-1/2 flex-1 flex items-center justify-between overflow-hidden text-neutral-1000  py-2 px-3 text-start text-sm font-normal font-display'
+						'group min-w-0 max-w-[160px] bg-neutral-100 h-1/2 flex-1 flex items-center justify-between overflow-hidden text-neutral-1000 dark:text-white  py-2 px-3 text-start text-sm font-normal font-display dark:bg-brand-dark'
 					)}
 					aria-current={tab.current ? 'page' : undefined}
 				>
-					<span>{tab.name}</span>
-					<PlusIcon className='rotate-45 inline stroke-brand w-4 h-4' />
+					<LDIcon />
+					<span className='w-3/4 h-fit ml-1 truncate'>{tab.name}</span>
+					<PlusIcon className='rotate-45 inline stroke-brand dark:stroke-brand-light w-4 h-4' />
 				</a>
 			))}
 		</nav>
