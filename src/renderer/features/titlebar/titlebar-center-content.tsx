5d3cc87936d4a802991da367a7109e979bdd8060
Joao GS Pereira <joaogspe@outlook.com>
2024-02-27 17:30:05 -0300

    :sparkles: feat: add the program name at titlebar

--- src/renderer/features/titlebar/titlebar-center-content.tsx
+++ src/renderer/features/titlebar/titlebar-center-content.tsx
@@ -1,10 +1,18 @@
+import { useLocation } from 'react-router-dom'
+import { OpenPLCIcon } from '~/renderer/assets/icons/oplc'
+
 export const TitlebarCenterContent = () => {
+	const navigation = useLocation()
+	const path = navigation.pathname
+
 	/**
 	 * TODO: Add the titlebar center content here based on the OS
 	 */
 	return (
-		<div className='oplc-titlebar-drag-region flex flex-1 items-center justify-center'>
-			{''}
+		<div className='oplc-titlebar-drag-region flex flex-1 items-center justify-center gap-2'>
+			{!path.includes('/workspace') && (
+				<span className='text-xs font-normal font-caption'>OpenPLC Editor</span>
+			)}
 		</div>
 	)
 }
--- src/renderer/features/titlebar/titlebar-left-content.tsx
+++ src/renderer/features/titlebar/titlebar-left-content.tsx
@@ -1,11 +1,18 @@
 import { OpenPLCIcon } from '~/renderer/assets/icons/oplc'
 import { MenuBar } from './menu/menu-bar'
+import { useLocation } from 'react-router-dom'
 
 export const TitlebarLeftContent = () => {
+	const navigation = useLocation()
+	const path = navigation.pathname
 	return (
 		<div className='flex items-center justify-start px-4 py-0.5 gap-1'>
-			<OpenPLCIcon />
-			<MenuBar />
+			{path.includes('/workspace') && (
+				<>
+					<OpenPLCIcon />
+					<MenuBar />
+				</>
+			)}
 		</div>
 	)
 }
