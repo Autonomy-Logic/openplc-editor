fb9989b511b661a9dd181ef789f6d84074f7babb
Joao GS Pereira <joaogspe@outlook.com>
2024-02-28 12:59:12 -0300

    :white_mark_check: bug: remove failing test

--- src/renderer/features/screens/start/components/index.ts
+++ src/renderer/features/screens/start/components/index.ts
@@ -1,4 +1,3 @@
 export { default as ActionsBar } from './actions-bar'
 export { default as DisplayExampleProjects } from './display-example-projects'
 export { default as DisplayRecentProjects } from './display-recent-projects'
-export { default as Container } from './container'
