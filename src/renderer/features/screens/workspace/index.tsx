fc419f73b013593f3049f75cae4e705169bdac5a
Joao GS Pereira <joaogspe@outlook.com>
2024-03-13 13:10:56 -0300

    :recycle: refactor: move comment code

--- src/renderer/features/screens/workspace/index.tsx
+++ src/renderer/features/screens/workspace/index.tsx
@@ -4,8 +4,8 @@ import { MainContent } from './components/main-content'
 export default function Workspace() {
 	return (
 		<div className='flex w-full h-full bg-brand-dark dark:bg-neutral-950'>
-			<Activitybar />
 			{/** Here goes the activity component */}
+			<Activitybar />
 			<MainContent />
 		</div>
 	)
