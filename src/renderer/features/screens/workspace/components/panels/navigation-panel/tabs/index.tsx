79dfcb9d4739c3e8a39fb38c00fa916d8fd4860a
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-03-19 16:00:38 -0300

    feat: create drag and drop function

--- src/renderer/features/screens/workspace/components/panels/navigation-panel/tabs/index.tsx
+++ src/renderer/features/screens/workspace/components/panels/navigation-panel/tabs/index.tsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { LDIcon, PlusIcon } from '@process:renderer/assets'
 import { cn } from '@utils/cn'
 
-const tabs = [
-	{ id: 1, name: 'Program', href: '#' },
-	{ id: 2, name: 'Function', href: '#' },
-	{ id: 3, name: 'Function Block', href: '#' },
-	{ id: 4, name: 'Data Type', href: '#' },
-]
-
 export const NavigationPanelTabs = () => {
+	const [tabs, setTabs] = useState([
+		{ id: 1, name: 'Program', href: '#' },
+		{ id: 2, name: 'Function', href: '#' },
+		{ id: 3, name: 'Function Block', href: '#' },
+		{ id: 4, name: 'Data Type', href: '#' },
+	])
 	const [selectedTab, setSelectedTab] = useState(tabs[0].id)
+
+	const dragTab = useRef<number>(0)
+	const draggedOverTab = useRef<number>(0)
+
+	const handleSort = () => {
+		const tabClone = [...tabs]
+		const draggedTab = tabClone[dragTab.current]
+		tabClone.splice(dragTab.current, 1)
+		tabClone.splice(draggedOverTab.current, 0, draggedTab)
+		setTabs(tabClone)
+	}
 
 	return (
 		<nav className='isolate flex border-none outline-none' aria-label='Tabs'>
-			{tabs.map((tab) => (
+			{tabs.map((tab, index) => (
 				<a
+					draggable
+					onDragStart={(e) => (dragTab.current = index)}
+					onDragEnter={(e) => (draggedOverTab.current = index)}
+					onDragEnd={() => handleSort()}
+					onDragOver={(e) => e.preventDefault()}
 					key={tab.id}
 					href={tab.href}
 					onClick={() => setSelectedTab(tab.id)}
@@ -24,7 +39,7 @@ export const NavigationPanelTabs = () => {
 							? ''
 							: 'opacity-[35%] border-r border-neutral-300',
 						'aria-[current=page]:dark:bg-brand-dark',
-						'group min-w-0 max-w-[160px] relative bg-neutral-100 h-1/2 flex-1 flex items-center justify-between overflow-hidden text-neutral-1000 dark:text-white py-2 px-3 text-start text-sm font-normal font-display dark:bg-neutral-800'
+						'group min-w-0 max-w-[160px] relative bg-neutral-100 relative h-1/2 flex-1 flex items-center justify-between overflow-hidden text-neutral-1000 dark:text-white py-2 px-3 text-start text-sm font-normal font-display dark:bg-neutral-800'
 					)}
 					aria-current={selectedTab === tab.id ? 'page' : undefined}
 				>
