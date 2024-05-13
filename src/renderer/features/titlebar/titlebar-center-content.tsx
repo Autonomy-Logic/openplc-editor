e29865bfb42ee11e41767dc97bf3af844c6bf4c6
JoaoGSPereira <joaogspe@outlook.com>
2024-05-13 13:53:38 -0300

    :bug: fix: fix data scope

--- src/renderer/features/titlebar/titlebar-center-content.tsx
+++ src/renderer/features/titlebar/titlebar-center-content.tsx
@@ -6,7 +6,7 @@ export const TitlebarCenterContent = () => {
   /**
    * Get the platform name from the store and check if it's macOS
    */
-  const OS = useOpenPLCStore().workspaceState.systemConfigs.OS
+  const OS = useOpenPLCStore().systemConfigs.OS
   const isMac = OS === 'darwin'
   /**
    * Get information about the current location to perform conditional rendering
