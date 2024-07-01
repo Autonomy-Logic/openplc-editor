4c23e352d31538ceb1f2645c4edccef0590d824b
Victor Le Roy <victorlrmatos@gmail.com>
2024-07-01 11:15:59 -0300

    feat: add new types and functions to editor slice

--- src/renderer/store/slices/editor/slice.ts
+++ src/renderer/store/slices/editor/slice.ts
@@ -4,23 +4,34 @@ import { StateCreator } from 'zustand'
 import type { EditorSlice, EditorState } from './types'
 
 export const createEditorSlice: StateCreator<EditorSlice, [], [], EditorSlice> = (setState) => ({
-  editor: {
-    type: 'available',
-    meta: {
-      name: 'name as string',
-    },
-  },
+  editor: [],
   editorActions: {
-    setEditor: ({ editor }) =>
+    addModel: (editor) =>
       setState(
         produce((state: EditorState) => {
-          state.editor = editor
+          const model = state.editor.find((model) => model.meta.name === editor.meta.name)
+          if (model) return
+          state.editor.push(editor)
+        }),
+      ),
+    removeModel: (name) =>
+      setState(
+        produce((state: EditorState) => {
+          state.editor = state.editor.filter((model) => model.meta.name !== name)
+        }),
+      ),
+    updateModelVariables: (name, variables) =>
+      setState(
+        produce((state: EditorState) => {
+          const model = state.editor.find((model) => model.meta.name === name)
+          if (model && (model.type === 'plc-textual' || model.type === 'plc-graphical')) model.variable = variables
+          return
         }),
       ),
     clearEditor: (): void =>
       setState(
         produce((state: EditorState) => {
-          state.editor = { type: 'available', meta: { name: 'name as string' } }
+          state.editor = []
         }),
       ),
   },
--- src/renderer/store/slices/editor/types.ts
+++ src/renderer/store/slices/editor/types.ts
@@ -1,64 +1,66 @@
 import z from 'zod'
 
+/** This is a zod schema for the variables.
+ * It is used to validate the variables if needed,
+ * in most cases you can use the type inferred from it.
+ */
+const editorVariablesSchema = z.discriminatedUnion('display', [
+  z.object({
+    display: z.literal('table'),
+    description: z.string(),
+    classFilter: z.enum(['All', 'Local', 'Input', 'Output', 'InOut', 'External', 'Temp']),
+    selectedRow: z.string(),
+  }),
+  z.object({ display: z.literal('code') }),
+])
+
+/** This is a zod schema for the model.
+ * It is used to validate the model if needed,
+ * in most cases you can use the type inferred from it.
+ */
+const editorModelSchema = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('plc-textual'),
+    meta: z.object({
+      name: z.string(),
+      path: z.string(),
+      language: z.enum(['il', 'st']),
+    }),
+    variable: editorVariablesSchema,
+  }),
+  z.object({
+    type: z.literal('plc-graphical'),
+    meta: z.object({
+      name: z.string(),
+      path: z.string(),
+      language: z.enum(['ld', 'sfc', 'fbd']),
+    }),
+    variable: editorVariablesSchema,
+  }),
+  z.object({
+    type: z.literal('plc-datatype'),
+    meta: z.object({
+      name: z.string(),
+      derivation: z.enum(['enumerated', 'structure', 'array']),
+    }),
+  }),
+  /**
+   * This must be reviewed in the future to fit the resource requirements
+   */
+  z.object({
+    type: z.literal('plc-resource'),
+    meta: z.object({
+      name: z.string(),
+    }),
+  }),
+])
+
 /** This is a zod schema for the editor slice state.
  * It is used to validate the editor data if needed,
  * in most cases you can use the type inferred from it.
  */
 const editorStateSchema = z.object({
-  editor: z.array(
-    z.discriminatedUnion('type', [
-      z.object({
-        type: z.literal('plc-textual'),
-        meta: z.object({
-          name: z.string(),
-          path: z.string(),
-          language: z.enum(['il', 'st']),
-        }),
-        variable: z.discriminatedUnion('display', [
-          z.object({
-            display: z.literal('table'),
-            description: z.string(),
-            classFilter: z.enum(['All', 'Local', 'Input', 'Output', 'InOut', 'External', 'Temp']),
-            selectedRow: z.string(),
-          }),
-          z.object({ display: z.literal('code') }),
-        ]),
-      }),
-      z.object({
-        type: z.literal('plc-graphical'),
-        meta: z.object({
-          name: z.string(),
-          path: z.string(),
-          language: z.enum(['ld', 'sfc', 'fbd']),
-        }),
-        variable: z.discriminatedUnion('display', [
-          z.object({
-            display: z.literal('table'),
-            description: z.string(),
-            classFilter: z.enum(['All', 'Local', 'Input', 'Output', 'InOut', 'External', 'Temp']),
-            selectedRow: z.string(),
-          }),
-          z.object({ display: z.literal('code') }),
-        ]),
-      }),
-      z.object({
-        type: z.literal('plc-datatype'),
-        meta: z.object({
-          name: z.string(),
-          derivation: z.enum(['enumerated', 'structure', 'array']),
-        }),
-      }),
-      /**
-       * This must be reviewed in the future to fit the resource requirements
-       */
-      z.object({
-        type: z.literal('plc-resource'),
-        meta: z.object({
-          name: z.string(),
-        }),
-      }),
-    ]),
-  ),
+  editor: z.array(editorModelSchema),
 })
 
 /** This is a zod schema for the editor slice actions.
@@ -66,10 +68,16 @@ const editorStateSchema = z.object({
  * in most cases you can use the type inferred from it.
  */
 const editorActionsSchema = z.object({
-  setEditor: z.function().args(editorStateSchema.shape.editor).returns(z.void()),
+  addModel: z.function().args(editorModelSchema).returns(z.void()),
+  removeModel: z.function().args(z.string()).returns(z.void()),
+  updateModelVariables: z.function().args(z.string(), editorVariablesSchema).returns(z.void()),
   clearEditor: z.function().returns(z.void()),
 })
 
+/** The variables, the data that we display in the app. */
+type VariablesTable = z.infer<typeof editorVariablesSchema>
+/** The model, the data that we display in the app. */
+type EditorModel = z.infer<typeof editorModelSchema>
 /** The state, the source of truth that drives our app. - Concept based on Redux */
 type EditorState = z.infer<typeof editorStateSchema>
 /** The actions, the events that occur in the app based on user input, and trigger updates in the state - Concept based on Redux */
@@ -78,6 +86,6 @@ type EditorSlice = EditorState & {
   editorActions: EditorActions
 }
 
-export { editorStateSchema }
+export { editorModelSchema, editorStateSchema }
 
-export type { EditorActions, EditorSlice, EditorState }
+export type { EditorActions, EditorModel, EditorSlice, EditorState, VariablesTable }
