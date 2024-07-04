d310c98751c60e41fb10ed2ef9136474792a566e
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-07-04 13:34:00 -0300

    feat: add visible table function on variable panel

--- src/renderer/screens/workspace-screen.tsx
+++ src/renderer/screens/workspace-screen.tsx
@@ -1,6 +1,6 @@
 import * as Tabs from '@radix-ui/react-tabs'
 import _ from 'lodash'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -75,9 +75,14 @@ const WorkspaceScreen = () => {
     { name: 'c', type: 'false' },
     { name: 'd', type: 'false' },
   ]
-
   const [isBottomBarCollapsed, setIsBottomBarCollapsed] = useState(false)
-  
+  const [isVariablesPanelCollapsed, setIsVariablesPanelCollapsed] = useState(false)
+  const panelRef = useRef(null)
+
+  const togglePanel = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    if (panelRef.current) panelRef.current.resize(25)
+  }
   return (
     <div className='flex h-full w-full bg-brand-dark dark:bg-neutral-950'>
       <WorkspaceSideContent>
@@ -144,21 +149,47 @@ const WorkspaceScreen = () => {
                           className='flex flex-1 flex-col gap-2'
                         >
                           <ResizablePanel
+                            ref={panelRef}
                             id='variableTablePanel'
                             order={1}
                             collapsible
+                            onCollapse={() => {
+                              setIsVariablesPanelCollapsed(true)
+                            }}
+                            onExpand={() => setIsVariablesPanelCollapsed(false)}
                             collapsedSize={0}
-                            minSize={20}
                             defaultSize={25}
-                            className='flex h-full w-full flex-1 flex-col gap-4 overflow-auto'
+                            minSize={20}
+                            className={`flex h-full w-full flex-1 flex-col gap-4 overflow-auto`}
                           >
                             <VariablesEditor />
                           </ResizablePanel>
-                          <ResizableHandle className='h-[1px] w-full bg-brand-light' />
+
+                          <ResizableHandle
+                            className={`${isVariablesPanelCollapsed && '!pointer-events-none !hidden !h-0 !w-0 !cursor-none !select-none !bg-none'} flex h-[1px] w-full bg-brand-light `}
+                          />
+
+                          {isVariablesPanelCollapsed && (
+                            <div className='flex w-full justify-center'>
+                              <button
+                                className='flex w-auto items-center gap-1 rounded-lg border-brand bg-neutral-50 px-2 py-1 dark:bg-neutral-900'
+                                onClick={togglePanel}
+                              >
+                                <p className='text-xs font-medium text-brand-medium dark:text-brand-light'>
+                                  Visible Table
+                                </p>
+                                <ExitIcon
+                                  size='sm'
+                                  className='-rotate-90 select-none fill-brand-medium  stroke-brand dark:fill-brand-light dark:stroke-brand-light'
+                                />
+                              </button>
+                            </div>
+                          )}
+
                           <ResizablePanel
+                            defaultSize={75}
                             id='textualEditorPanel'
                             order={2}
-                            defaultSize={75}
                             className='mt-6 flex-1 flex-grow rounded-md'
                           >
                             <MonacoEditor
