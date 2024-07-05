522f7b92f2a1ff915bdf755f0d2a723baa0b9b7a
JoaoGS <joaogspe@outlook.com>
2024-07-05 10:52:50 -0300

    :recycle: refactor: change datatypes icons

--- src/renderer/assets/icons/project/Array.tsx
+++ src/renderer/assets/icons/project/Array.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@utils/cn'
 import { ComponentPropsWithoutRef } from 'react'
 
-type IArrayIconProps = ComponentPropsWithoutRef<'svg'> & {
+type ArrayIconProps = ComponentPropsWithoutRef<'svg'> & {
   size?: 'sm' | 'md' | 'lg'
 }
 
@@ -11,7 +11,7 @@ const sizeClasses = {
   lg: 'w-12 h-12',
 }
 
-export const ArrayIcon = (props: IArrayIconProps) => {
+export const ArrayIcon = (props: ArrayIconProps) => {
   const { className, size = 'sm', ...res } = props
   return (
     <svg
@@ -21,94 +21,88 @@ export const ArrayIcon = (props: IArrayIconProps) => {
       className={cn(`${sizeClasses[size]}`, className)}
       {...res}
     >
-      <title> Array Icon</title>
+      <title>Array Icon</title>
       <path
-        d='M26.0005 19.3333V11.1667C26.0005 8.58934 23.8515 6.5 21.2005 6.5H18.0005C16.9619 6.5 15.9514 6.1725 15.1205 5.56667L12.8805 3.93333C12.0496 3.3275 11.0391 3 10.0005 3H6.80049C4.14952 3 2.00049 5.08934 2.00049 7.66667V19.3333C2.00049 21.9107 4.14952 24 6.80049 24H21.2005C23.8515 24 26.0005 21.9107 26.0005 19.3333Z'
+        opacity='0.4'
+        d='M3.49988 20.9997V6.99968C3.49988 4.42235 5.58922 2.33301 8.16655 2.33301L15.1665 2.33302L24.4999 11.6664V20.9997C24.4999 23.577 22.4105 25.6664 19.8332 25.6664H8.16654C5.58922 25.6664 3.49988 23.577 3.49988 20.9997Z'
         fill='#023C97'
       />
-      <g clipPath='url(#clip0_2002_48412)'>
-        <path
-          d='M12.8105 17.1716H15.1899'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          d='M16.0821 16.2794V13.9'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M15.1899 13.3052V12.7104C15.1899 12.3819 15.4562 12.1156 15.7847 12.1156H16.3795C16.708 12.1156 16.9744 12.3819 16.9744 12.7104V13.3052C16.9744 13.6337 16.708 13.9 16.3795 13.9H15.7847C15.4562 13.9 15.1899 13.6337 15.1899 13.3052Z'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M16.9744 16.8742V17.469C16.9744 17.7975 16.708 18.0638 16.3795 18.0638H15.7847C15.4562 18.0638 15.1899 17.7975 15.1899 17.469V16.8742C15.1899 16.5457 15.4562 16.2794 15.7847 16.2794H16.3795C16.708 16.2794 16.9744 16.5457 16.9744 16.8742Z'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          d='M15.1899 13.0078H12.8105'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          d='M11.918 16.2794V13.9'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M11.0259 13.3052V12.7104C11.0259 12.3819 11.2922 12.1156 11.6207 12.1156H12.2155C12.544 12.1156 12.8103 12.3819 12.8103 12.7104V13.3052C12.8103 13.6337 12.544 13.9 12.2155 13.9H11.6207C11.2922 13.9 11.0259 13.6337 11.0259 13.3052Z'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M12.8103 16.8742V17.469C12.8103 17.7975 12.544 18.0638 12.2155 18.0638H11.6207C11.2922 18.0638 11.0259 17.7975 11.0259 17.469V16.8742C11.0259 16.5457 11.2922 16.2794 11.6207 16.2794H12.2155C12.544 16.2794 12.8103 16.5457 12.8103 16.8742Z'
-          stroke='white'
-          strokeWidth='0.746567'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <rect
-          opacity='0.4'
-          x='8.64697'
-          y='9.73657'
-          width='10.7065'
-          height='10.7065'
-          rx='2.97278'
-          stroke='#B4D0FE'
-          strokeWidth='0.891834'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-      </g>
-      <defs>
-        <clipPath id='clip0_2002_48412'>
-          <rect width='18' height='18' fill='white' transform='translate(23.0005 4.99994) rotate(90)' />
-        </clipPath>
-      </defs>
+      <path
+        d='M15.1661 6.99977V2.3331L24.4995 11.6664H19.8328C17.2555 11.6664 15.1661 9.5771 15.1661 6.99977Z'
+        fill='#023C97'
+      />
+      <path
+        d='M12.1501 18.2652H13.8497'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M14.4868 17.6273V15.9277'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M13.8496 15.503V15.0782C13.8496 14.8435 14.0398 14.6533 14.2745 14.6533H14.6993C14.934 14.6533 15.1242 14.8435 15.1242 15.0782V15.503C15.1242 15.7377 14.934 15.9279 14.6993 15.9279H14.2745C14.0398 15.9279 13.8496 15.7377 13.8496 15.503Z'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M15.1242 18.0528V18.4776C15.1242 18.7123 14.934 18.9025 14.6993 18.9025H14.2745C14.0398 18.9025 13.8496 18.7123 13.8496 18.4776V18.0528C13.8496 17.8181 14.0398 17.6279 14.2745 17.6279H14.6993C14.934 17.6279 15.1242 17.8181 15.1242 18.0528Z'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M13.8497 15.291H12.1501'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M11.5127 17.6273V15.9277'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M10.8755 15.503V15.0782C10.8755 14.8435 11.0657 14.6533 11.3003 14.6533H11.7252C11.9598 14.6533 12.1501 14.8435 12.1501 15.0782V15.503C12.1501 15.7377 11.9598 15.9279 11.7252 15.9279H11.3003C11.0657 15.9279 10.8755 15.7377 10.8755 15.503Z'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M12.1501 18.0528V18.4776C12.1501 18.7123 11.9598 18.9025 11.7252 18.9025H11.3003C11.0657 18.9025 10.8755 18.7123 10.8755 18.4776V18.0528C10.8755 17.8181 11.0657 17.6279 11.3003 17.6279H11.7252C11.9598 17.6279 12.1501 17.8181 12.1501 18.0528Z'
+        stroke='white'
+        strokeWidth='0.533262'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        opacity='0.4'
+        d='M14.7001 12.9541H11.2994C10.1267 12.9541 9.17603 13.9048 9.17603 15.0775V18.4782C9.17603 19.6509 10.1267 20.6016 11.2994 20.6016H14.7001C15.8728 20.6016 16.8235 19.6509 16.8235 18.4782V15.0775C16.8235 13.9048 15.8728 12.9541 14.7001 12.9541Z'
+        stroke='#B4D0FE'
+        strokeWidth='0.637024'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
     </svg>
   )
 }
