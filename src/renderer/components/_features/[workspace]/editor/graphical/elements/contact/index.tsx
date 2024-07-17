512cdc897d35d612c1af759121ebe24aeb9bacc3
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-07-17 10:59:03 -0300

    feat: create modifier options

--- src/renderer/components/_features/[workspace]/editor/graphical/elements/contact/index.tsx
+++ src/renderer/components/_features/[workspace]/editor/graphical/elements/contact/index.tsx
@@ -7,26 +7,29 @@ const ContactELement = () => {
       <ModalContent onClose={() => {}} className='h-[468px] w-[413px] flex-col gap-8 px-6 py-4'>
         <ModalTitle className='text-xl font-medium text-neutral-950 dark:text-white'>Block Properties</ModalTitle>
         <div className='flex h-[316px] w-full gap-10'>
-          <div className='h-full w-full bg-red-500'>
+          <div className='relative h-full w-full text-sm font-medium text-neutral-950'>
             <span>Modifier</span>
-            <ul className='mt-4 flex flex-col gap-4'>
-              <li>Normal</li>
-              <li>Negated</li>
-              <li>Rising Edge</li>
-              <li>Falling Edge</li>
+            <ul className='mt-4 flex flex-col gap-4 '>
+              {['Normal', 'Negated', 'Rising Edge', 'Falling Edge'].map((modifier, index) => (
+                <li key={index} className='flex items-center'>
+                  <input type='radio' name='modifier' id={modifier} className='mr-2' />
+                  <label htmlFor={modifier}>{modifier}</label>
+                </li>
+              ))}
             </ul>
-            <div className='mt-full'>
+
+            <div className='absolute bottom-0 w-full'>
               <p>Variable</p>
-              <select></select>
+              <select className='h-4 w-full'></select>
             </div>
           </div>
-          <div className='h-full w-full bg-green-500'></div>
+          <div className='h-full w-full'></div>
         </div>
         <div className='flex !h-8 w-full gap-1'>
-          <button className={`h-full w-full items-center rounded-lg bg-brand text-center font-medium text-white `}>
+          <button className='h-full w-full items-center rounded-lg bg-brand text-center font-medium text-white'>
             Ok
           </button>
-          <button className='h-full w-full items-center rounded-lg  bg-neutral-100 text-center font-medium text-neutral-1000 dark:bg-neutral-850 dark:text-neutral-100'>
+          <button className='h-full w-full items-center rounded-lg bg-neutral-100 text-center font-medium text-neutral-1000 dark:bg-neutral-850 dark:text-neutral-100'>
             Cancel
           </button>
         </div>
