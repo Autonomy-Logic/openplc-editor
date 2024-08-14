3804448448893c0dab2ad694cca050b96f9c32ce
Joao GS Pereira <joaogspe@outlook.com>
2024-08-14 12:22:50 -0300

    :construction: wip: create base schema for ladder data

---
+++ src/types/PLC/language-data/ladder-diagram.ts
@@ -0,0 +1,28 @@
+/**
+ * ----------------------------------------------------------------
+ * This file is part of OpenPLC documentation.
+ * Here is defined the shape of the mapped data structure for the Ladder Diagram language.
+ * ----------------------------------------------------------------
+*/
+
+import { z } from 'zod'
+
+const powerRails = z.object({
+    left: z.array(z.object({
+        x: z.number(),
+        y: z.number(),
+        width: z.number(),
+        height: z.number(),
+        label: z.string(),
+        data: z.object({
+            handles: z.array(z.object({
+                x: z.number(),
+                y: z.number(),
+                type: z.enum(['left', 'right']),
+            })),
+        }),
+    })),
+    right: z.array(z.object({
+        // Same structure as left power rails
+    })),
+})
