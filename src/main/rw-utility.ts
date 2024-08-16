b445907543b701057607e431f6eb4fb5c4dfc8c4
Joao GS Pereira <joaogspe@outlook.com>
2024-08-16 15:57:54 -0300

    :construction: wip: introduce new function to create an Editor folder

--- src/main/rw-utility.ts
+++ src/main/rw-utility.ts
@@ -3,7 +3,6 @@ import { access, constants, mkdir, writeFile } from 'fs/promises'
 import { join } from 'path'
 
 const UserSettings = {
-  userSettingsFolderPath: join(app.getPath('userData'), 'User'),
   /**
    * Checks if the user base settings folder exists and creates it if it doesn't.
    * Also creates a user settings file with default values if it doesn't exist.
@@ -12,14 +11,16 @@ const UserSettings = {
    */
   checkIfUserBaseSettingsExists: async function (): Promise<void> {
     // Construct the user folder path.
-    const pathToUserData = this.userSettingsFolderPath
+    const pathToUserDataFolder = join(app.getPath('userData'), 'User')
+    // Construct the user settings file path.
+    const pathToUserDataFile = join(pathToUserDataFolder, 'settings.json')
     // Check if user folder exists. If not, create it.
     try {
-      await access(pathToUserData, constants.F_OK)
-      console.log(`User setting folder found at ${pathToUserData}`)
+      await access(pathToUserDataFolder, constants.F_OK)
+      console.log(`User setting folder found at ${pathToUserDataFolder}`)
     } catch (_err) {
       try {
-        const settingsFolder = await mkdir(pathToUserData, { recursive: true })
+        const settingsFolder = await mkdir(pathToUserDataFolder, { recursive: true })
         console.log(`User setting folder created at ${settingsFolder}`)
         return
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -32,7 +33,7 @@ const UserSettings = {
       // Check if the user setting file exists. If not, create it.
       try {
         await writeFile(
-          join(pathToUserData, 'settings.json'),
+          pathToUserDataFile,
           JSON.stringify(
             {
               'theme-preference': 'light',
@@ -51,7 +52,7 @@ const UserSettings = {
           // This flag opens the file to writing and creates if it doesn't exist, and fails if it already exists.
           { flag: 'wx' },
         )
-        console.log(`User setting file created at ${join(pathToUserData, 'settings.json')}`)
+        console.log(`User setting file created at ${pathToUserDataFile}`)
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (err: any) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
@@ -61,4 +62,33 @@ const UserSettings = {
   },
 }
 
-export { UserSettings }
+const EditorSettings = {
+  /**
+   * Checks if the editor folder exists and creates it if it doesn't.
+   * Also creates an editor file with default values if it doesn't exist.
+   *
+   * @returns {Promise<void>} Resolves when the editor folder are ready.
+   */
+  checkIfEditorFolderExists: async function (): Promise<void> {
+    // Construct the editor folder path.
+    const pathToEditorFolder = join(app.getPath('userData'), 'Editor')
+    // Check if editor folder exists. If not, create it.
+    try {
+      await access(pathToEditorFolder, constants.F_OK)
+      console.log(`Editor folder found at ${pathToEditorFolder}`)
+    } catch (_err) {
+      try {
+        const editorFolder = await mkdir(pathToEditorFolder, { recursive: true })
+        console.log(`Editor folder created at ${editorFolder}`)
+        return
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (_err: any) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        console.error(`Error creating editor folder: ${_err.message}`)
+        return
+      }
+    }
+  },
+}
+
+export { EditorSettings, UserSettings }
