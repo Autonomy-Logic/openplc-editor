2244e4ad979061f9931b94e2472843575ff20e26
KaiqueNoronha <kaiquesic@hotmail.com>
2024-09-10 13:13:40 -0300

    feat: adjust project container size with fixed min and max width

--- src/renderer/components/_organisms/explorer/project.tsx
+++ src/renderer/components/_organisms/explorer/project.tsx
@@ -4,6 +4,7 @@ import { useOpenPLCStore } from '@root/renderer/store'
 import { TabsProps } from '@root/renderer/store/slices'
 // import { CreateEditorObject } from '@root/renderer/store/slices/shared/utils'
 import { CreateEditorObjectFromTab } from '@root/renderer/store/slices/tabs/utils'
+import React, { useState } from 'react'
 
 import { CreatePLCElement } from '../../_features/[workspace]/create-element'
 
@@ -12,6 +13,7 @@ const Project = () => {
     workspace: {
       projectData: { pous, dataTypes },
     },
+    // projectName
     tabsActions: { updateTabs },
     editorActions: { setEditor, addModel, getEditorFromEditors },
   } = useOpenPLCStore()
@@ -45,27 +47,49 @@ const Project = () => {
     addModel(editor)
     setEditor(editor)
   }
+  const [isEditing, setIsEditing] = useState(false)
+  const [projectNameDefault, setProjectNameDefault] = useState('name')
 
+  const handleBlur = () => {
+    setIsEditing(false)
+  }
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProjectNameDefault(e.target.value)
+  }
   return (
     <div className='w-full'>
       {/* Actions handler */}
-      <div id='project-actions-container' className='relative z-10 my-3 flex w-full justify-normal gap-2 px-2'>
-        <div
-          id='project-name-container'
-          className='flex h-8 w-full flex-1 cursor-default select-none items-center justify-start gap-1 rounded-lg bg-neutral-100 px-1.5 dark:bg-brand-dark'
-        >
-          <FolderIcon size='sm' />
-          <span
-            id='project-name'
-            className='pl-1 font-caption text-xs font-medium text-neutral-1000 dark:text-neutral-50'
-          >
-            {Name}
-          </span>
-        </div>
-        <div id='create-plc-container'>
-          <CreatePLCElement />
-        </div>
-      </div>
+      <div id='project-actions-container' className='relative z-10 my-3 flex w-full justify-between gap-2 px-2'>
+  <div
+    id='project-name-container'
+    className='flex h-8 flex-1 cursor-default select-none items-center justify-start gap-1 rounded-lg bg-neutral-100 px-1.5 py-[1px] dark:bg-brand-dark'
+    onDoubleClick={() => setIsEditing(true)}
+  >
+    <FolderIcon size='sm' />
+    {isEditing ? (
+      <input
+        id='project-name'
+        className='w-full cursor-text rounded-lg border border-neutral-700 bg-transparent px-2 py-1 text-xs font-medium text-neutral-1000 outline-none dark:text-neutral-50'
+        value={projectNameDefault}
+        onChange={handleNameChange}
+        onBlur={handleBlur}
+        autoFocus
+      />
+    ) : (
+      <p
+        id='project-name'
+        className='min-w-[20px] max-w-[250px] cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 py-1 text-xs font-medium text-neutral-1000 dark:text-neutral-50'
+      >
+        {projectNameDefault}
+      </p>
+    )}
+  </div>
+  <div id='create-plc-container'>
+    <CreatePLCElement />
+  </div>
+</div>
+
+
       {/* Data display */}
       <ProjectTreeRoot label={Name}>
         <ProjectTreeBranch branchTarget='function'>
