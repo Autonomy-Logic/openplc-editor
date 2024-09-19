ce8fdbb1a9e49951a402f70a0cc26c9c54b82cb9
Daniel <danielnorges@gmail.com>
2024-09-19 14:28:55 -0300

    FEAT: implements modal structure with progress bar and interactive buttons

--- src/renderer/components/_features/[start]/new-project/index.tsx
+++ src/renderer/components/_features/[start]/new-project/index.tsx
@@ -1,6 +1,6 @@
 import { BookIcon, FolderIcon } from '@root/renderer/assets'
 import { Modal, ModalContent } from '@root/renderer/components/_molecules'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface NewProjectModalProps {
   isOpen: boolean
@@ -14,12 +14,19 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({ isOpen, onClos
     setSelected(value)
   }
 
+  // Reset the state when the modal closes
+  useEffect(() => {
+    if (!isOpen) {
+      setSelected(null)
+    }
+  }, [isOpen])
+
   return (
     <Modal open={isOpen} onOpenChange={onClose}>
       <ModalContent onClose={onClose}>
-        <div className='flex h-full w-full flex-col items-center justify-between'>
+        <div className='flex h-full  w-full flex-col items-center '>
           {/* Progress Bar */}
-          <div className='relative flex h-[100px] items-center justify-center'>
+          <div className='relative flex h-[100px] items-center justify-center mb-8'>
             {/* Step 1 */}
             <div className='relative flex items-center'>
               {/* Outer Circle */}
@@ -32,10 +39,7 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({ isOpen, onClos
             </div>
 
             {/* Line between step 1 and step 2 */}
-            <div className='space-y-1'>
-              <div className='h-[2px] w-12 bg-blue-500'></div>
-              <div className='h-[2px] w-12 bg-blue-500'></div>
-            </div>
+              <div className='h-[4px] w-12 bg-blue-500'></div>
             {/* Step 2 */}
             <div className='flex items-center'>
               <div className='bg-custom-gray-light z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500 bg-[#edeff2] font-bold text-blue-500'>
@@ -44,10 +48,8 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({ isOpen, onClos
             </div>
 
             {/* Line between step 2 and step 3 */}
-            <div className='space-y-1'>
-              <div className='h-[2px] w-12 bg-blue-500'></div>
-              <div className='h-[2px] w-12 bg-blue-500'></div>
-            </div>
+            <div className='h-[4px] w-12 bg-blue-500'></div>
+
 
             {/* Step 3 */}
             <div className='flex items-center'>
@@ -57,28 +59,41 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({ isOpen, onClos
             </div>
           </div>
 
-          <h2 className='mb-4 text-center text-lg font-semibold'>What type of project you will be working on?</h2>
-          <div className='flex w-full justify-around bg-white'>
+          <h2 className=' text-center text-lg font-semibold mb-14'>What type of project you will be working on?</h2> {/* Adjusted margin-bottom */}
+
+          <div className='flex w-full justify-around bg-white mb-16'>
             <button
-              className={`flex h-12 w-40 items-center justify-center rounded-md border-2  ${selected === 'project' ? 'border-blue-500 bg-blue-500 text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
+              className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'project' ? 'border-blue-300 bg-blue-300 text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
               onClick={() => handleClick('project')}
             >
               <FolderIcon className='mr-2' /> PLC Project
             </button>
             <button
-              className={`flex h-12 w-40 items-center justify-center rounded-md border-2  ${selected === 'library' ? 'border-blue-500 bg-blue-500  text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
+              className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'library' ? 'border-blue-300 bg-blue-300  text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
               onClick={() => handleClick('library')}
             >
               <BookIcon className='mr-2' /> Library
             </button>
           </div>
-          <div className='flex flex-row justify-center space-x-4'>
-            <button className='flex h-8 w-48 items-center justify-center rounded-md bg-[#0464fb] px-4 py-2 font-semibold text-white shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:shadow-xl active:scale-95 active:shadow-inner'>
-              Cancel
-            </button>
-            <button className='flex h-8 w-48  items-center justify-center rounded-md border-2 border-[#0464fb] bg-white px-4 py-2  font-semibold text-black shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:border-[#0350c9] hover:shadow-xl active:scale-95 active:shadow-inner'>
+
+          <div className='flex flex-col justify-center space-y-4 '>
+          <button
+              className={`flex h-8 w-52 items-center justify-center rounded-md border-2 border-[#0464fb] bg-white px-4 py-2 font-semibold text-black shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:border-[#0350c9] hover:shadow-xl active:scale-95 active:shadow-inner ${
+                !selected ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              type='button'
+              disabled={!selected}
+            >
               Next
             </button>
+            <button
+              className='flex h-8 w-52 items-center justify-center rounded-md bg-[#0464fb] px-4 py-2 font-semibold text-white shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:shadow-xl active:scale-95 active:shadow-inner'
+              type='button'
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+
           </div>
         </div>
       </ModalContent>
