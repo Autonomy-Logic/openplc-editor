f0237aedc5644310db19084360f5d93844cef3a2
Daniel <danielnorges@gmail.com>
2024-09-19 16:24:55 -0300

    FEAT:  Added `disabled:hover` class to ensure hover effects are not applied to buttons when they are disabled.

--- src/renderer/components/_features/[start]/new-project/index.tsx
+++ src/renderer/components/_features/[start]/new-project/index.tsx
@@ -24,76 +24,62 @@ export const NewProjectModal: React.FC<NewProjectModalProps> = ({ isOpen, onClos
   return (
     <Modal open={isOpen} onOpenChange={onClose}>
       <ModalContent onClose={onClose}>
-        <div className='flex h-full  w-full flex-col items-center '>
+        <div className='flex h-full  w-full flex-col items-center justify-around '>
           {/* Progress Bar */}
-          <div className='relative flex h-[100px] items-center justify-center mb-8'>
-            {/* Step 1 */}
-            <div className='relative flex items-center'>
-              {/* Outer Circle */}
-              <div className='relative flex h-14 w-14 items-center justify-center rounded-full border-2 border-blue-500'>
-                {/* Inner Circle */}
-                <div className='absolute flex h-12 w-12 items-center justify-center rounded-full border-2 border-[#0464fb] bg-[#0464fb] font-bold text-[#d0e2fe]'>
-                  1
-                </div>
-              </div>
+          <div className='relative mb-8 flex h-[100px] items-center justify-center'>
+            <div className='z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500 bg-white font-bold text-blue-500 '>
+              1
             </div>
-
-            {/* Line between step 1 and step 2 */}
-              <div className='h-[4px] w-12 bg-blue-500'></div>
-            {/* Step 2 */}
+            <div className='h-[2px] w-12 bg-gray-500'></div>
             <div className='flex items-center'>
-              <div className='bg-custom-gray-light z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500 bg-[#edeff2] font-bold text-blue-500'>
+              <div className='z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-200 font-bold text-gray-500'>
                 2
               </div>
             </div>
 
-            {/* Line between step 2 and step 3 */}
-            <div className='h-[4px] w-12 bg-blue-500'></div>
+            <div className='h-[2px] w-12 bg-gray-500'></div>
 
-
-            {/* Step 3 */}
             <div className='flex items-center'>
-              <div className='bg-custom-gray-light z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-blue-500 bg-[#edeff2] font-bold text-blue-500'>
+              <div className='z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-200 font-bold text-gray-500'>
                 3
               </div>
             </div>
           </div>
-
-          <h2 className=' text-center text-lg font-semibold mb-14'>What type of project you will be working on?</h2> {/* Adjusted margin-bottom */}
-
-          <div className='flex w-full justify-around bg-white mb-16'>
-            <button
-              className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'project' ? 'border-blue-300 bg-blue-300 text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
-              onClick={() => handleClick('project')}
-            >
-              <FolderIcon className='mr-2' /> PLC Project
-            </button>
-            <button
-              className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'library' ? 'border-blue-300 bg-blue-300  text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
-              onClick={() => handleClick('library')}
-            >
-              <BookIcon className='mr-2' /> Library
-            </button>
+          <div>
+            <h2 className=' mb-14 text-center text-lg font-semibold'>What type of project you will be working on?</h2>{' '}
+            <div className='mb-16 flex w-full justify-around '>
+              <button
+                className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'project' ? 'border-blue-300 bg-blue-300 text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
+                onClick={() => handleClick('project')}
+              >
+                <FolderIcon className='mr-2' /> PLC Project
+              </button>
+              <button
+                className={`flex h-10 w-40 items-center justify-center rounded-md border-2  ${selected === 'library' ? 'border-blue-300 bg-blue-300  text-white' : 'border-transparent bg-gray-200 text-black hover:border-blue-500'}`}
+                onClick={() => handleClick('library')}
+              >
+                <BookIcon className='mr-2' /> Library
+              </button>
+            </div>
           </div>
 
-          <div className='flex flex-col justify-center space-y-4 '>
-          <button
-              className={`flex h-8 w-52 items-center justify-center rounded-md border-2 border-[#0464fb] bg-white px-4 py-2 font-semibold text-black shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:border-[#0350c9] hover:shadow-xl active:scale-95 active:shadow-inner ${
-                !selected ? 'opacity-50 cursor-not-allowed' : ''
+          <div className='flex flex-row justify-center space-x-4'>
+            <button
+              className='flex h-8 w-52 items-center justify-center rounded-md bg-neutral-100 font-semibold !text-neutral-1000 hover:bg-neutral-200 disabled:hover:bg-neutral-100 dark:bg-white dark:hover:bg-neutral-100'
+              type='button'
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            <button
+              className={`flex h-8 w-52 items-center justify-center rounded-md bg-brand font-semibold !text-white hover:bg-brand-medium-dark focus:bg-brand-medium ${
+                !selected ? 'cursor-not-allowed opacity-50 disabled:hover:bg-brand' : ''
               }`}
               type='button'
               disabled={!selected}
             >
               Next
             </button>
-            <button
-              className='flex h-8 w-52 items-center justify-center rounded-md bg-[#0464fb] px-4 py-2 font-semibold text-white shadow-[0_4px_4px_rgba(0,_0,_0,_0.25)] transition-all duration-300 hover:shadow-xl active:scale-95 active:shadow-inner'
-              type='button'
-              onClick={onClose}
-            >
-              Cancel
-            </button>
-
           </div>
         </div>
       </ModalContent>
