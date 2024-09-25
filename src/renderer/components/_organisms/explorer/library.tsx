8dbe2888d30d109d8045d0a1c9b16b1dd7fdcf1c
Matheus Damasceno Jaques <matheusdamascenodev@gmail.com>
2024-09-25 16:20:34 -0300

    FEAT - create animation for input component

--- src/renderer/components/_organisms/explorer/library.tsx
+++ src/renderer/components/_organisms/explorer/library.tsx
@@ -1,6 +1,6 @@
-import { BookIcon, MagnifierIcon } from '@root/renderer/assets'
+import { BookIcon, CloseIcon, MagnifierIcon } from '@root/renderer/assets'
 import { useOpenPLCStore } from '@root/renderer/store'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 import { InputWithRef } from '../../_atoms'
 import { LibraryFile, LibraryFolder, LibraryRoot } from '../../_molecules'
@@ -26,6 +26,19 @@ const Library = () => {
   const [selectedFileKey, setSelectedFileKey] = useState<string | null>(null)
   const [isSearchActive, setIsSearchActive] = useState(false)
   const [filterText, setFilterText] = useState('')
+  const [shouldRenderInput, setShouldRenderInput] = useState(false)
+
+  useEffect(() => {
+    if (isSearchActive) {
+      setShouldRenderInput(true)
+    } else {
+      const timer = setTimeout(() => {
+        setShouldRenderInput(false)
+      }, 500)
+
+      return () => clearTimeout(timer)
+    }
+  }, [isSearchActive])
 
   const filteredLibraries = system.filter((library) =>
     library.pous.some((pou) => pou.name.toLowerCase().includes(filterText)),
@@ -56,23 +69,46 @@ const Library = () => {
 
         <div
           id='search-container'
-          className={`flex h-8 items-center rounded-lg bg-brand ${isSearchActive ? 'w-full' : 'w-10'}`}
+          className={`relative flex h-8 items-center justify-start overflow-hidden rounded-lg transition-all duration-500 ease-in-out ${isSearchActive ? 'w-full' : 'w-10'}`}
         >
-          {isSearchActive && (
-            <InputWithRef
-              id='Search-File'
-              type='text'
-              placeholder='Search'
-              className='h-8 w-full flex-grow rounded-s-lg bg-neutral-100 px-2 focus:outline-none dark:bg-brand-dark'
-              value={filterText}
-              onChange={handleFilterChange}
-            />
+          {shouldRenderInput && (
+            <div
+              className={`absolute left-0 flex w-full items-center justify-between bg-neutral-100 transition-all duration-500 ease-in-out dark:bg-brand-dark ${
+                isSearchActive ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <InputWithRef
+                id='Search-File'
+                type='text'
+                placeholder='Search'
+                className='h-8 w-full px-2 bg-neutral-100 font-caption text-xs font-medium focus:outline-none dark:bg-brand-dark'
+                value={filterText}
+                onChange={handleFilterChange}
+              />
+            </div>
           )}
-          <div className='flex-shrink-0 w-10 flex items-center justify-center'>
-            <MagnifierIcon className='w-6' onClick={() => setIsSearchActive(!isSearchActive)} />
+          <div className='absolute right-0 flex h-8 w-10 flex-shrink-0 items-center justify-center bg-brand'>
+            {isSearchActive ? (
+              <CloseIcon
+                className='w-4 cursor-pointer stroke-white'
+                onClick={() => {
+                  setFilterText('')
+                  setIsSearchActive(false)
+                }}
+              />
+            ) : (
+              <MagnifierIcon
+                className='w-6 cursor-pointer'
+                onClick={() => {
+                  setIsSearchActive(!isSearchActive)
+                  setFilterText('')
+                }}
+              />
+            )}
           </div>
         </div>
       </div>
+
       {/* Data display */}
       <div id='library-tree-container' className='flex h-full w-full flex-col overflow-auto pr-1'>
         <LibraryRoot>
