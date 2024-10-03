79d777ba9a357c92d72a2ca92370644eb5761063
Victor Le Roy <victorlrmatos@gmail.com>
2024-10-03 14:25:36 -0300

    feat: change library functionality to create function blocks at ST

---
+++ src/renderer/components/_features/[workspace]/editor/monaco/drag-and-drop/st.ts
@@ -0,0 +1,26 @@
+export const parsePouToStText = (pou: {
+  name: string
+  language: string
+  type: string
+  body: string
+  documentation: string
+  variables: {
+    name: string
+    class: string
+    type: { definition: string; value: string }
+  }[]
+}) => {
+  const inputVariables = pou.variables
+    .filter((variable) => variable.class === 'input')
+    .map((variable) => {
+      return `${variable.name} := (*${variable.type.value}*)`
+    })
+  const outputVariables = pou.variables
+    .filter((variable) => variable.class === 'output')
+    .map((variable) => {
+      return `${variable.name} => (*${variable.type.value}*)`
+    })
+  const lastOutputVariable = outputVariables.pop()
+
+  return `${pou.name} (\n\t${inputVariables.length > 0 ? inputVariables.join(',\n\t') + ',\n\t' : ''}${outputVariables.length > 0 ? outputVariables.join(',\n\t') + ',\n\t' : ''}${lastOutputVariable};\n)`
+}
