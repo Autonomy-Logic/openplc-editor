7ea8524861aab7240adfe417597b967a1e8779dc
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-10-16 08:16:16 -0300

    fix: start screen display recents layout

--- src/renderer/components/refactor/display-recent-projects/index.tsx
+++ src/renderer/components/refactor/display-recent-projects/index.tsx
@@ -128,10 +128,80 @@ const DisplayRecentProjects = (props: IDisplayRecentProjectProps) => {
 
   return (
     <section className='flex h-[52%] w-full flex-col pr-9 2xl:h-3/5 3xl:h-3/4 4xl:h-4/5 4xl:pr-0' {...props}>
-      <h2 className='mb-6 flex w-full flex-1 cursor-default justify-start font-caption text-xl font-medium text-neutral-1000 dark:text-white'>
+      <h2 className='mb-6 flex  cursor-default justify-start font-caption text-xl font-medium text-neutral-1000 dark:text-white'>
         Projects
       </h2>
-      <div className='scroll-area flex h-full w-full gap-4'>
+      <div className='scroll-area flex h-auto w-full gap-[25px]  flex-wrap overflow-y-auto'>
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
+        {recentProjects.map((project) => (
+          <FileElement.Root
+            onClick={() => void handleOpenProject(project.path)}
+            className='overflow-hidden '
+            key={project.path}
+          >
+            <FileElement.Label projectName={project.path} lastModified={projectTimes[project.path]} />
+            <FileElement.Shape />
+          </FileElement.Root>
+        ))}
         {recentProjects.map((project) => (
           <FileElement.Root
             onClick={() => void handleOpenProject(project.path)}
