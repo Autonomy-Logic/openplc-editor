43fbf89379c5a92bcfb012d91a91c3d56afe7e76
Daniel <danielnorges@gmail.com>
2024-10-17 14:55:46 -0300

    feat: create new service and utils for create project

---
+++ src/main/services/project-service/utils/create-project-file.ts
@@ -0,0 +1,73 @@
+import { PLCProject } from '../../../../types/PLC/open-plc'
+import { CreateJSONFile } from '../../../utils'
+
+type CreateProjectFileProps = {
+  language: 'il' | 'st' | 'ld' | 'sfc' | 'fbd'
+  time: string
+  type: 'plc-project' | 'plc-library'
+  name: string
+  path: string
+}
+const CreateProjectFile = (dataToCreateProjectFile: CreateProjectFileProps) => {
+  const _projectJSONStructure: PLCProject = {
+    meta: {
+      name: dataToCreateProjectFile.name,
+      type: dataToCreateProjectFile.type as 'plc-project',
+    },
+    data: {
+      pous: [
+        {
+          type: 'program',
+          data: {
+            name: 'main',
+            language: dataToCreateProjectFile.language,
+            variables: [],
+            documentation: '',
+            body: {
+              // Bad code!!!!!
+              language: dataToCreateProjectFile.language as 'il',
+              value: '',
+            },
+          },
+        },
+      ],
+      dataTypes: [],
+      configuration: {
+        resource: {
+          tasks: [
+            {
+              name: 'task0',
+              triggering: 'Cyclic',
+              interval: dataToCreateProjectFile.time,
+              priority: 1,
+              id: '0',
+            },
+          ],
+          instances: [
+            {
+              name: 'instance0',
+              program: 'main',
+              task: 'task0',
+              id: '0',
+            },
+          ],
+          globalVariables: [],
+        },
+      },
+    },
+  }
+
+  CreateJSONFile(dataToCreateProjectFile.path, JSON.stringify(_projectJSONStructure, null, 2), 'project')
+
+  return {
+    success: true,
+    data: {
+      meta: {
+        path: dataToCreateProjectFile.path,
+      },
+      content: _projectJSONStructure,
+    },
+  }
+}
+
+export { CreateProjectFile }
