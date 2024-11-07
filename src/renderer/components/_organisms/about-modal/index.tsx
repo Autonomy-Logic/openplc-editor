12cd2dac378bdc27ceb858ad4c905e6080ea9629
MarcosGAC <marcosgabriel.dev@gmail.com>
2024-11-07 14:31:57 -0300

    feat: add openPLC editor logo on about modal

--- src/renderer/components/_organisms/about-modal/index.tsx
+++ src/renderer/components/_organisms/about-modal/index.tsx
@@ -1,5 +1,7 @@
+import openPlcLogo from '@root/renderer/assets/icons/about/logo.svg'
 import { Modal, ModalContent, ModalTitle } from '@root/renderer/components/_molecules'
 import { useOpenPLCStore } from '@root/renderer/store'
+
 
 const AboutModal = () => {
   const {
@@ -14,44 +16,48 @@ const AboutModal = () => {
   const isAboutModalOpen = isModalOpen.some(
     (modal: { modalName: string; modalState: boolean }) => modal.modalName === 'aboutOpenPlc' && modal.modalState,
   )
-  const handleModalClose = () => {
+  const closeModal = () => {
     setModalOpen('aboutOpenPlc', false)
   }
+  const title = 'OpenPLC Editor 3.0'
+  const releaseDate = 'Release: 2024-10-16'
+  const description = 'Open Source IDE for the OpenPLC Runtime, compliant with the IEC 61131-3 international standard.'
+  const basedOn =
+    'Based on PLCOpen Editor and Beremiz by Andrey Skvortsov, Sergey Surkov, Edouard Tisserant, and Laurent Bessard.'
+  const copyright = '(C) 2019 Thiago Alves'
+  const linkUrl = 'https://autonomylogic.com/'
+
   return (
     <Modal onOpenChange={handleOpenChange} open={isAboutModalOpen}>
       <ModalContent className='h-[540px] w-[508px] select-none flex-col justify-between px-4 py-4'>
-        <ModalTitle className='text-xs font-normal text-neutral-950 dark:text-white'>About OpenPLC</ModalTitle>
-        <div className='h-[180px] w-full bg-blue-500'></div>
-        <div className='text-xs font-normal dark:text-neutral-200'>
-          <div className='w-full items-center text-center text-xl font-bold'>
-            <h2>OpenPLC Editor 3.0</h2>
-            <h2>Release: 2024-10-16</h2>
-          </div>
-
-          <p>Open Source IDE for the OpenPLC Runtime, compliant with the IEC 61131-3 international standard.</p>
-          <br />
-          <p>
-            Based on PLCOpen Editor and Beremiz by Andrey Skvortsov, Sergey Surkov, Edourard Tisserant and Laurent
-            Bessard.
-          </p>
-          <p className='w-full items-center text-center'>(C) 2019 Thiago Alves</p>
-          <p className='w-full items-center text-center'>
-            <a className='text-blue-500 underline' href='https://autonomylogic.com/'>https://autonomylogic.com/</a>
-          </p>
+        <ModalTitle className='  text-neutral-950 dark:text-white'>About OpenPLC Editor</ModalTitle>
+        <div className='flex h-[180px] w-full items-center justify-center bg-[#0464fb]'>
+          <img src={openPlcLogo} />
         </div>
-        <div className='flex w-full justify-center gap-3 font-medium text-sm dark:text-neutral-100'>
-          <button className='h-8 w-20 rounded-md bg-neutral-100 dark:bg-neutral-850' onClick={() => handleModalClose()}>
-            credits
-          </button>
-          <button className='h-8 w-20 rounded-md bg-neutral-100 dark:bg-neutral-850' onClick={() => handleModalClose()}>
-            license
-          </button>
-          <button className='h-8 w-20 rounded-md bg-neutral-100 dark:bg-neutral-850' onClick={() => handleModalClose()}>
-            sponsors
-          </button>
-          <button className='h-8 w-20 rounded-md bg-neutral-100 dark:bg-neutral-850' onClick={() => handleModalClose()}>
-            Close
-          </button>
+
+        <div className='text-center text-xs font-normal dark:text-neutral-200'>
+          <div className='text-xl font-bold'>
+            <h2>{title}</h2>
+            <h2>{releaseDate}</h2>
+          </div>
+          <div className='text-left'>
+            <p className='mt-4'>{description}</p>
+            <p className='mt-2'>{basedOn}</p>
+          </div>
+            <p className='mt-2'>{copyright}</p>
+            <p className='mt-2'>
+              <a className='text-[#0464fb] underline' href={linkUrl}>
+                {linkUrl}
+              </a>
+            </p>
+        </div>
+
+        <div className='mt-4 flex justify-center gap-3 text-sm font-medium dark:text-neutral-100'>
+          {['Credits', 'License', 'Sponsors', 'Close'].map((label, index) => (
+            <button key={index} className='h-8 w-20 rounded-md bg-neutral-100 dark:bg-neutral-850' onClick={closeModal}>
+              {label}
+            </button>
+          ))}
         </div>
       </ModalContent>
     </Modal>
