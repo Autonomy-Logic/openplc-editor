5a8ba4ee7637ed686c007f54a0e0f6e6a40e1c5d
Thiago Alves <admin@autonomylogic.com>
2024-11-12 14:10:14 -0500

    Revert back changes

--- src/utils/PLC/pou-xml.ts
+++ src/utils/PLC/pou-xml.ts
@@ -1,103 +1,147 @@
-import { ProjectState } from '@root/renderer/store/slices'
-import { BaseXml } from '@root/types/PLC/xml-data/base-diagram'
-import { create } from 'xmlbuilder2'
+// import { PLCVariable } from '@root/types/PLC'
+import { RungState } from '@root/renderer/store/slices'
+import { PLCPou } from '@root/types/PLC/open-plc'
+import { BaseXml } from '@root/types/PLC/xml-data'
+import { InterfaceXML } from '@root/types/PLC/xml-data/pous/interface/interface-diagram'
+import { VariableXML } from '@root/types/PLC/xml-data/variable/variable-diagram'
 
-import formatDate from '../formatDate'
-import { instanceToXml } from './instances-xml'
-import { parsePousToXML } from './pou-xml'
+import { ilToXML } from './language/il-xml'
+import { ladderToXml } from './language/ladder-xml'
+import { stToXML } from './language/st-xml'
 
-const baseXmlStructure = (): BaseXml => ({
-  project: {
-    '@xmlns': 'http://www.plcopen.org/xml/tc6_0201',
-    '@xmlns:xsd': 'http://www.w3.org/2001/XMLSchema-instance',
-    '@xmlns:xhtml': 'http://www.w3.org/1999/xhtml',
-    '@xmlns:ns1': 'http://www.plcopen.org/xml/tc6_0201',
-    fileHeader: {
-      '@companyName': 'Unknown',
-      '@productName': 'Unnamed',
-      '@productVersion': '1',
-      '@creationDateTime': formatDate(new Date()),
-    },
-    contentHeader: {
-      '@name': 'Unnamed',
-      '@modificationDateTime': formatDate(new Date()),
-      coordinateInfo: {
-        fbd: {
-          scaling: {
-            '@x': '10',
-            '@y': '10',
-          },
-        },
-        ld: {
-          scaling: {
-            '@x': '10',
-            '@y': '10',
-          },
-        },
-        sfc: {
-          scaling: {
-            '@x': '10',
-            '@y': '10',
-          },
-        },
+export const parseInterface = (pou: PLCPou) => {
+  const variables = pou.data.variables
+  const returnType = pou.type === 'function' ? pou.data.returnType : undefined
+
+  const xml: InterfaceXML = {}
+  variables.forEach((variable) => {
+    const v: VariableXML = {
+      '@name': variable.name,
+      type: {
+        // !BAD CODE
+        [variable.type.definition === 'derived' ? 'derived' : variable.type.value.toUpperCase()]:
+          variable.type.definition === 'derived' ? { '@name': variable.type.value } : '',
       },
-    },
+    }
 
-    types: {
-      dataTypes: '',
-      pous: {
-        pou: [],
-      },
-    },
+    if (variable.location) v['@address'] = variable.location
 
-    instances: {
-      configurations: {
-        configuration: {
-          '@name': 'Config0',
-          resource: {
-            '@name': 'Res0',
-            task: [],
-          },
+    if (variable.initialValue) v.initialValue = variable.initialValue
+
+    if (variable.documentation)
+      v.documentation = {
+        'xhtml:p': {
+          $: variable.documentation,
         },
-      },
-    },
-  },
-})
-
-export const parseProjectToXML = (project: ProjectState) => {
-  console.log('=-=-=-= PARSING TO XML =-=-=-=')
-  console.log('Project:', project)
-  let xmlResult = baseXmlStructure()
-
-  /**
-   * Parse POUs
-   */
-  const pous = project.data.pous
-  xmlResult = parsePousToXML(xmlResult, pous)
-
-  /**
-   * Parse instances
-   */
-  const configuration = project.data.configuration
-  xmlResult = instanceToXml(xmlResult, configuration)
-
-  const doc = create(xmlResult)
-  doc.dec({ version: '1.0', encoding: 'utf-8' })
-  const xml = doc.end({ prettyPrint: true })
-  console.log('xml as object', xmlResult)
-  console.log(xml)
-  console.log('=-=-=-= FINISHED PARSE TO XML =-=-=-=')
-  console.log('=-=-=-= SAVING XML =-=-=-=')
-  window.bridge
-    .writeXMLFile(project.meta.path.replace('project.json', ''), xml, 'plc')
-    .then((res) => {
-      if (res) {
-        console.log('File saved', project.meta.path.replace('project.json', 'plc.xml'))
-        console.log('=-=-=-= FINISHED SAVING XML =-=-=-=')
       }
-    })
-    .catch((err) => {
-      console.error('Error saving project:', err)
-      console.log('=-=-=-= FINISHED SAVING XML =-=-=-=')
-    })
+
+    if (returnType) {
+      if (!xml.returnType) xml.returnType
+      xml.returnType = {
+        [returnType]: '',
+      }
+    }
+
+    switch (variable.class) {
+      case 'input': {
+        if (!xml.inputVars) xml.inputVars = { variable: [] }
+        if (!xml.inputVars.variable) xml.inputVars.variable = []
+        xml.inputVars.variable.push(v)
+        return
+      }
+      case 'output': {
+        if (!xml.outputVars) xml.outputVars = { variable: [] }
+        if (!xml.outputVars.variable) xml.outputVars.variable = []
+        xml.outputVars.variable.push(v)
+        return
+      }
+      case 'inOut': {
+        if (!xml.inOutVars) xml.inOutVars = { variable: [] }
+        if (!xml.inOutVars.variable) xml.inOutVars.variable = []
+        xml.inOutVars.variable.push(v)
+        return
+      }
+      case 'external': {
+        if (!xml.externalVars) xml.externalVars = { variable: [] }
+        if (!xml.externalVars.variable) xml.externalVars.variable = []
+        xml.externalVars.variable.push(v)
+        return
+      }
+      case 'local': {
+        if (!xml.localVars) xml.localVars = { variable: [] }
+        if (!xml.localVars.variable) xml.localVars.variable = []
+        xml.localVars.variable.push(v)
+        return
+      }
+      case 'temp': {
+        if (!xml.tempVars) xml.tempVars = { variable: [] }
+        if (!xml.tempVars.variable) xml.tempVars.variable = []
+        xml.tempVars.variable.push(v)
+        return
+      }
+      default:
+        return
+    }
+  })
+
+  return xml
+}
+
+export const parsePousToXML = (xml: BaseXml, pous: PLCPou[]) => {
+  pous.forEach((pou) => {
+    const interfaceResult = parseInterface(pou)
+
+    switch (pou.data.body.language) {
+      case 'il': {
+        const result = ilToXML(pou.data.body.value)
+        xml.project.types.pous.pou.push({
+          '@name': pou.data.name,
+          '@pouType': pou.type === 'function-block' ? 'functionBlock' : pou.type,
+          interface: interfaceResult,
+          body: result.body,
+          documentation: {
+            'xhtml:p': {
+              $: pou.data.documentation,
+            },
+          },
+        })
+        return
+      }
+      case 'st': {
+        const result = stToXML(pou.data.body.value)
+        xml.project.types.pous.pou.push({
+          '@name': pou.data.name,
+          '@pouType': pou.type === 'function-block' ? 'functionBlock' : pou.type,
+          interface: interfaceResult,
+          body: result.body,
+          documentation: {
+            'xhtml:p': {
+              $: pou.data.documentation,
+            },
+          },
+        })
+        return
+      }
+      case 'ld': {
+        const rungs = pou.data.body.value.rungs
+        const result = ladderToXml(rungs as RungState[])
+        xml.project.types.pous.pou.push({
+          '@name': pou.data.name,
+          '@pouType': pou.type === 'function-block' ? 'functionBlock' : pou.type,
+          interface: interfaceResult,
+          body: result.body,
+          documentation: {
+            'xhtml:p': {
+              $: pou.data.documentation,
+            },
+          },
+        })
+        return
+      }
+      default:
+        return
+    }
+  })
+
+  return xml
 }
