8a9d6fa76a9bdfafc66a27a5abd281b96c515da2
Victor Le Roy <victorlrmatos@gmail.com>
2024-11-13 14:03:13 -0300

    fix: CreateProjectSlice bodyData.language to bodyData.derivation

--- src/main/services/utils/create-project-file.ts
+++ src/main/services/utils/create-project-file.ts
@@ -1,5 +1,5 @@
-import { PLCProject } from '../../../../types/PLC/open-plc'
-import { CreateJSONFile } from '../../../utils'
+import { PLCProject } from '../../../../src/types/PLC/open-plc'
+import { CreateJSONFile } from '../../../main/utils/'
 
 type CreateProjectFileProps = {
   language: 'il' | 'st' | 'ld' | 'sfc' | 'fbd'
@@ -11,8 +11,8 @@ type CreateProjectFileProps = {
 const CreateProjectFile = (dataToCreateProjectFile: CreateProjectFileProps) => {
   const bodyData =
     dataToCreateProjectFile.language === 'ld'
-      ? { language: dataToCreateProjectFile.language, value: { name: dataToCreateProjectFile.name, rungs: [] } }
-      : { language: dataToCreateProjectFile.language, value: 'This is the body' }
+      ? { derivation: dataToCreateProjectFile.language, value: { name: dataToCreateProjectFile.name, rungs: [] } }
+      : { derivation: dataToCreateProjectFile.language, value: 'This is the body' }
 
   const _projectJSONStructure: PLCProject = {
     meta: {
