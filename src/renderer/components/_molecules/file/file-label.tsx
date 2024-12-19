bddca19ac53618a9a7b27f81613b2c4c743cc68a
Victor Le Roy <victorlrmatos@gmail.com>
2024-12-19 11:49:53 -0300

    feat: add name to recent files at start menu

--- src/renderer/components/_molecules/file/file-label.tsx
+++ src/renderer/components/_molecules/file/file-label.tsx
@@ -3,25 +3,40 @@ import { HTMLAttributes, ReactElement } from 'react'
 import { Label as FileLabel } from '../../_atoms'
 type FolderLabelProps = HTMLAttributes<HTMLParagraphElement> & {
   projectName?: string
-  lastModified?: string 
+  projectPath?: string
+  lastModified?: string
 }
 
-export default function Label({ projectName, lastModified, ...props }: FolderLabelProps): ReactElement {
+export default function Label({ projectName, projectPath, lastModified, ...props }: FolderLabelProps): ReactElement {
+  const truncateStart = (str: string | undefined, maxLength: number): string => {
+    if (!str) return ''
+    const strNoProjectJson = str.replace('/project.json', '')
+    return strNoProjectJson.length > maxLength ? '...' + strNoProjectJson.slice(-maxLength) : strNoProjectJson
+  }
+
+  console.log('projectName', projectName, 'projectPath', projectPath, 'lastModified', lastModified)
+
   return (
     <p
       id={projectName}
-      className='absolute bottom-4  flex w-full cursor-pointer flex-col gap-[1px] overflow-hidden px-3 text-xs text-white'
+      className='absolute bottom-4 flex w-full cursor-pointer flex-col gap-[1px] overflow-hidden px-3 text-white'
       {...props}
     >
       <FileLabel
         label={projectName}
         id={projectName}
-        className=' w-full text-ellipsis text-white overflow-hidden  whitespace-nowrap '
+        className='w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold text-white'
+      />
+      <FileLabel
+        label={truncateStart(projectPath, 30)}
+        id={projectPath}
+        title={projectPath}
+        className='w-full overflow-hidden text-ellipsis whitespace-nowrap text-[0.6rem] text-white'
       />
       <FileLabel
         label={lastModified}
         id={lastModified}
-        className='overflow-hidden w-full text-white whitespace-nowrap text-[10px] opacity-40 '
+        className='w-full overflow-hidden whitespace-nowrap text-[0.625rem] text-white opacity-40'
       />
     </p>
   )
--- src/renderer/components/_molecules/file/file-root.tsx
+++ src/renderer/components/_molecules/file/file-root.tsx
@@ -9,5 +9,5 @@ type FolderRootProps = HTMLAttributes<HTMLDivElement>
 export default function Root({ ...props }: FolderRootProps) {
   const defaultStyle = 'flex relative w-[224px] h-[160px]'
   const { className } = props
-  return <div title='file-root' id='folder-root' {...props} className={cn(defaultStyle, className)} />
+  return <div aria-label='file-root' id='folder-root' {...props} className={cn(defaultStyle, className)} />
 }
