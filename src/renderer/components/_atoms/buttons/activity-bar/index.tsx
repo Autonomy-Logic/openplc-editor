a2ecda53297c0dee1a3acd523ced4f7acd76655d
MarcosGAC <marcosgabriel.dev@gmail.com>
2025-02-27 12:35:06 -0300

    fix: button warning

--- src/renderer/components/_atoms/buttons/activity-bar/index.tsx
+++ src/renderer/components/_atoms/buttons/activity-bar/index.tsx
@@ -1,13 +1,15 @@
 import { ComponentPropsWithoutRef } from 'react'
 
-type IActivityBarButtonProps = ComponentPropsWithoutRef<'button'>
+type IActivityBarButtonProps = ComponentPropsWithoutRef<'div'>
 
 const ActivityBarButton = (props: IActivityBarButtonProps) => {
   const { children, ...res } = props
   return (
-    <button type='button' className='flex h-6 w-full items-center justify-center' {...res}>
-      <div className=' transition-all duration-150 rounded-md p-[6px] hover:bg-brand-medium dark:hover:bg-neutral-900'>{children}</div>
-    </button>
+    <div role='button'  className='flex h-6 w-full cursor-pointer items-center justify-center' {...res}>
+      <div className='rounded-md p-[6px] transition-all duration-150 hover:bg-brand-medium dark:hover:bg-neutral-900'>
+        {children}
+      </div>
+    </div>
   )
 }
 
