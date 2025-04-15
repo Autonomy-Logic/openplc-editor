7289d07aff0bc914b1d9471b4bd869055b1798e1
Joao GS Pereira <joaogspe@outlook.com>
2025-04-15 12:16:53 -0300

    :sparkles: add board specs

--- src/renderer/components/_features/[workspace]/editor/device/elements/board-configuration/index.tsx
+++ src/renderer/components/_features/[workspace]/editor/device/elements/board-configuration/index.tsx
@@ -14,8 +14,6 @@ const BoardConfiguration = () => {
     deviceActions: { setDeviceBoard, setCommunicationPort, setAvailableOptions },
   } = useOpenPLCStore()
 
-  console.log('Boards', availableBoards)
-
   const refreshCommunicationPorts = async (e: React.MouseEvent) => {
     e.preventDefault()
     try {
@@ -32,8 +30,11 @@ const BoardConfiguration = () => {
 
   return (
     <div id='board-configuration-container' className='flex h-full w-1/2 flex-col gap-6'>
-      <div id='board-figure-container' className='h-[60%]'></div>
-      <div id='board-specs-container' className='flex h-[40%]  flex-col items-start justify-center gap-3 p-32 sm:p-16'>
+      <div id='board-figure-container' className='h-[55%]'></div>
+      <div
+        id='board-preferences-container'
+        className='flex h-[45%] flex-col items-start justify-center gap-3 p-32 sm:p-16'
+      >
         <div id='board-selection' className='flex items-center justify-center gap-1'>
           <SelectField
             label='Device'
@@ -41,6 +42,7 @@ const BoardConfiguration = () => {
             setSelectedOption={setDeviceBoard}
             options={Array.from(availableBoards.keys())}
             ariaLabel='Device selection'
+            className={availableBoards.get(deviceBoard)?.isCoreInstalled ? '[&_span]:opacity-60' : ''}
           />
         </div>
         <div id='programming-port-selection' className='flex items-center justify-center gap-1'>
@@ -56,7 +58,14 @@ const BoardConfiguration = () => {
             <RefreshIcon size='sm' className={isPressed ? 'spin-refresh' : ''} />
           </button>
         </div>
-        <div id='board-specs'>Here goes the specs</div>
+        <p className='text-start font-caption text-xs font-semibold text-neutral-850 dark:text-white'>Specs</p>
+        <div id='board-specs-container' className='grid grid-cols-2 place-content-around gap-2 overflow-auto'>
+          {Object.entries(availableBoards.get(deviceBoard)?.specs || {}).map(([spec, value]) => (
+            <p className='text-start font-caption text-cp-sm font-semibold text-neutral-850 dark:text-white' key={spec}>
+              {spec}: <span className='font-light text-neutral-600 dark:text-neutral-400'>{value}</span>
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   )
