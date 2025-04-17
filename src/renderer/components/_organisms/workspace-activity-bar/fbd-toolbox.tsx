9fad3c28bae9b22c922ef1d90707316a24e5f59f
Victor Le Roy <victorlrmatos@gmail.com>
2025-04-17 13:03:51 -0300

    feat: add TooltipButton component and enhance FBD/Ladder toolboxes with tooltips

--- src/renderer/components/_organisms/workspace-activity-bar/fbd-toolbox.tsx
+++ src/renderer/components/_organisms/workspace-activity-bar/fbd-toolbox.tsx
@@ -1,4 +1,11 @@
-import { BlockButton, Connector, Continuation, InputVariable, OutVariable } from '../../_molecules/workspace-activity-bar/fbd'
+import {
+  BlockButton,
+  Connector,
+  Continuation,
+  InputVariable,
+  OutVariable,
+} from '../../_molecules/workspace-activity-bar/fbd'
+import { TooltipButton } from '../../_molecules/workspace-activity-bar/tooltip-button'
 
 export const FBDToolbox = () => {
   const handleDragStart = (event: React.DragEvent<HTMLDivElement>, iconType: string) => {
@@ -8,11 +15,21 @@ export const FBDToolbox = () => {
 
   return (
     <>
-      <BlockButton onDragStart={(event) => handleDragStart(event, 'block')} />
-      <InputVariable onDragStart={(event) => handleDragStart(event, 'input-variable')} />
-      <OutVariable onDragStart={(event) => handleDragStart(event, 'output-variable')} />
-      <Connector onDragStart={(event) => handleDragStart(event, 'connector')} />
-      <Continuation onDragStart={(event) => handleDragStart(event, 'continuation')} />
+      <TooltipButton tooltipContent='Block'>
+        <BlockButton onDragStart={(event) => handleDragStart(event, 'block')} />
+      </TooltipButton>
+      <TooltipButton tooltipContent='Input Variable'>
+        <InputVariable onDragStart={(event) => handleDragStart(event, 'input-variable')} />
+      </TooltipButton>
+      <TooltipButton tooltipContent='Output Variable'>
+        <OutVariable onDragStart={(event) => handleDragStart(event, 'output-variable')} />
+      </TooltipButton>
+      <TooltipButton tooltipContent='Connector'>
+        <Connector onDragStart={(event) => handleDragStart(event, 'connector')} />
+      </TooltipButton>
+      <TooltipButton tooltipContent='Continuation'>
+        <Continuation onDragStart={(event) => handleDragStart(event, 'continuation')} />
+      </TooltipButton>
     </>
   )
 }
