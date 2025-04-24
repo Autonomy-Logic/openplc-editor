a10dfb7deda8d95bc7fd4bd68e9eb26e85e49b73
Joao GS Pereira <joaogspe@outlook.com>
2025-04-24 18:26:35 -0300

    :tada: create branch for pin mapping

---
+++ src/renderer/components/_molecules/pin-mapping-table/index.tsx
@@ -0,0 +1,9 @@
+const PinMappingTable = () => {
+  return (
+    <div id='Pin mapping table container'>
+      <p>Here goes the table components</p>
+    </div>
+  )
+}
+
+export { PinMappingTable }
