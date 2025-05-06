674e8beea0873542e66b4d4ecc100549a9f9caec
João GS Pereira <joaogspe@outlook.com>
2025-05-06 08:56:57 -0300

    :construction: create slot components

--- src/renderer/components/_templates/[editors]/device-editor-template.tsx
+++ src/renderer/components/_templates/[editors]/device-editor-template.tsx
@@ -1,7 +1,20 @@
 import { ComponentPropsWithoutRef } from 'react'
 
 type DeviceEditorTemplateProps = ComponentPropsWithoutRef<'div'>
-const DeviceEditorTemplate = (props: DeviceEditorTemplateProps) => {
+
+const _DeviceEditorLeftSlot = (props: ComponentPropsWithoutRef<'div'>) => {
   return <div {...props}>{props.children}</div>
+}
+
+const _DeviceEditorRightSlot = (props: ComponentPropsWithoutRef<'div'>) => {
+  return <div {...props}>{props.children}</div>
+}
+
+const DeviceEditorTemplate = (props: DeviceEditorTemplateProps) => {
+  return (
+    <div id='device-editor-template' {...props}>
+      {props.children}
+    </div>
+  )
 }
 export { DeviceEditorTemplate }
