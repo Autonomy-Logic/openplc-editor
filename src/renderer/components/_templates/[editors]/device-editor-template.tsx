2ad0bedc7c04678438484c87296ecc1debe6e9af
João GS Pereira <joaogspe@outlook.com>
2025-05-06 21:06:16 -0300

    :recycle: improve user experience

--- src/renderer/components/_templates/[editors]/device-editor-template.tsx
+++ src/renderer/components/_templates/[editors]/device-editor-template.tsx
@@ -10,10 +10,10 @@ const DeviceEditorLeftSlot = (props: DeviceEditorSlotProps) => {
   return (
     <div
       id='device-editor-left-slot'
-      className='flex h-full w-1/2 min-w-[325px] flex-col overflow-y-auto overflow-x-hidden p-4 lg:min-w-[625px] lg:p-8'
+      className='flex h-full w-1/2 min-w-[325px] flex-col gap-4 overflow-y-auto overflow-x-hidden p-4 lg:min-w-[625px] lg:px-8 lg:py-4'
       {...props}
     >
-      <h2 id='slot-title' className='text-lg font-medium text-neutral-950 dark:text-white'>
+      <h2 id='slot-title' className='select-none text-lg font-medium text-neutral-950 dark:text-white'>
         {props.heading}
       </h2>
       {props.children}
@@ -25,10 +25,10 @@ const DeviceEditorRightSlot = (props: DeviceEditorSlotProps) => {
   return (
     <div
       id='device-editor-right-slot'
-      className='flex h-full w-1/2 min-w-[325px] flex-col overflow-y-auto overflow-x-hidden p-4 lg:min-w-[625px] lg:p-8'
+      className='flex h-full w-1/2 min-w-[325px] flex-col gap-4 overflow-y-auto overflow-x-hidden p-4 lg:min-w-[625px] lg:px-8 lg:py-4'
       {...props}
     >
-      <h2 id='slot-title' className='text-lg font-medium text-neutral-950 dark:text-white'>
+      <h2 id='slot-title' className='select-none text-lg font-medium text-neutral-950 dark:text-white'>
         {props.heading}
       </h2>
       {props.children}
