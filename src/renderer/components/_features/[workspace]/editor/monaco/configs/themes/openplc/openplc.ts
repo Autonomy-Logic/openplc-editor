5d520337e17697744756ee41f1d5ed726e5e574d
JoaoGSP <joaogspe@outlook.com>
2025-07-03 13:40:25 -0300

    chore: add warning flag for monaco implementation

--- src/renderer/components/_features/[workspace]/editor/monaco/configs/themes/openplc/openplc.ts
+++ src/renderer/components/_features/[workspace]/editor/monaco/configs/themes/openplc/openplc.ts
@@ -10,7 +10,7 @@ const lightThemeData: monaco.editor.IStandaloneThemeData = {
     { token: 'label.il', foreground: '#0464FB' }, // Primary color for label (identifier) in light theme
     { token: 'labelValue', foreground: '#023C97' }, // Slightly darker shade for label (value) in light theme
     { token: 'st.keyword', foreground: '#C586C0' }, // Pink for keywords in ST language in light theme
-    { token: 'variable', foreground: '#000000' }, // Black for keywords in ST language in dark theme
+    { token: 'variable', foreground: '#000000' }, // Black for keywords in ST language in dark theme - WARNING: This is a workaround for a bad design choice, we need to fix this in the monaco editor implementation
   ],
   colors: {
     'editor.background': '#FFFFFF', // Editor background color in light theme
@@ -34,7 +34,7 @@ const darkThemeData: monaco.editor.IStandaloneThemeData = {
     { token: 'label.il', foreground: '#0464FB' }, // Primary color for label (identifier) in dark theme
     { token: 'labelValue', foreground: '#023C97' }, // Slightly darker shade for label (value) in dark theme
     { token: 'st.keyword', foreground: '#C586C0' }, // Pink for keywords in ST language in dark theme
-    { token: 'variable', foreground: '#FFFFFF' }, // White for keywords in ST language in dark theme
+    { token: 'variable', foreground: '#FFFFFF' }, // White for keywords in ST language in dark theme - WARNING: This is a workaround for a bad design choice, we need to fix this in the monaco editor implementation
   ],
   colors: {
     'editor.background': '#121316', // Darker background color in dark theme
