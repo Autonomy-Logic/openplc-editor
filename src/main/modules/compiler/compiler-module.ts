89a0fb725cc0cdbec2d1b2372748af5aa50d5257
JoaoGSP <joaogspe@outlook.com>
2025-07-12 20:13:56 -0300

    :recycle: refactor CompilerModule to improve path handling and add CLI availability checks

--- src/main/modules/compiler/compiler-module.ts
+++ src/main/modules/compiler/compiler-module.ts
@@ -1,9 +1,14 @@
+import { exec } from 'node:child_process'
 import { cp, mkdir } from 'node:fs/promises'
+import os from 'node:os'
 import { join } from 'node:path'
+import { promisify } from 'node:util'
+
+import { app as electronApp } from 'electron'
 
 // import type { ArduinoCliConfig, BoardInfo, HalsFile } from './compiler-types'
 
-interface BuildStepResult {
+interface MethodsResult {
   success: boolean
   data?: unknown
 }
@@ -12,14 +17,30 @@ class CompilerModule {
   binaryDirectoryPath: string
   sourceDirectoryPath: string
 
+  arduinoCliBinaryPath: string
+  arduinoCliConfigurationFilePath: string
+  arduinoCliBaseParameters: string[]
+
+  iec2cBinaryPath: string
+
+  // ############################################################################
+  // =========================== Static properties ==============================
+  // ############################################################################
+  static readonly HOST_PLATFORM = process.platform
+  static readonly HOST_ARCHITECTURE = process.arch
+  static readonly DEVELOPMENT_MODE = process.env.NODE_ENV === 'development'
+
   constructor() {
     this.binaryDirectoryPath = this.#constructBinaryDirectoryPath()
     this.sourceDirectoryPath = this.#constructSourceDirectoryPath()
-  }
 
-  // ############################################################################
-  // =========================== Static methods ================================
-  // ############################################################################
+    this.arduinoCliBinaryPath = this.#constructArduinoCliBinaryPath()
+    this.arduinoCliConfigurationFilePath = join(electronApp.getPath('userData'), 'User', 'arduino-cli.yaml')
+    // INFO: We use this approach because some commands can receive additional parameters as a string arrayÍ.
+    this.arduinoCliBaseParameters = ['--config-file', this.arduinoCliConfigurationFilePath]
+
+    this.iec2cBinaryPath = this.#constructIec2cBinaryPath()
+  }
 
   // ############################################################################
   // =========================== Private methods ================================
@@ -27,72 +48,130 @@ class CompilerModule {
 
   // Initialize paths based on the environment
   #constructBinaryDirectoryPath(): string {
-    const isDevelopment = process.env.NODE_ENV === 'development'
-    return join(isDevelopment ? process.cwd() : process.resourcesPath, isDevelopment ? 'resources' : '', 'bin')
+    return join(
+      CompilerModule.DEVELOPMENT_MODE ? process.cwd() : process.resourcesPath,
+      CompilerModule.DEVELOPMENT_MODE ? 'resources' : '',
+      'bin',
+    )
   }
 
   #constructSourceDirectoryPath(): string {
-    const isDevelopment = process.env.NODE_ENV === 'development'
-    return join(isDevelopment ? process.cwd() : process.resourcesPath, isDevelopment ? 'resources' : '', 'sources')
+    return join(
+      CompilerModule.DEVELOPMENT_MODE ? process.cwd() : process.resourcesPath,
+      CompilerModule.DEVELOPMENT_MODE ? 'resources' : '',
+      'sources',
+    )
   }
 
-  //   async #initializeBuildEnvironment(): Promise<void> {}
+  // TODO: Validate the path.
+  #constructArduinoCliBinaryPath(): string {
+    return join(this.binaryDirectoryPath, 'arduino-cli', 'arduino-cli')
+  }
+
+  // TODO: Validate the path.
+  #constructIec2cBinaryPath(): string {
+    return join(this.binaryDirectoryPath, 'iec2c', 'iec2c')
+  }
 
   // ############################################################################
   // =========================== Public methods =================================
   // ############################################################################
 
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // ++ ========================= Build Steps ================================= ++
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++ ========================= Utility methods ============================= ++
 
-  // ========================= Initialization Methods =========================
-  async createDirectories(projectFolderPath: string, boardTarget: string): Promise<BuildStepResult> {
+  getHostHardwareInfo() {
+    return `
+      System Architecture: ${process.arch},
+      Operating System: ${process.platform},
+      Processor: ${process.env.PROCESSOR_IDENTIFIER},
+      Logical CPU Cores: ${os.cpus().length},
+      CPU Frequency: ${os.cpus()[0].speed} MHz,
+      CPU Model: ${os.cpus()[0].model},
+    `
+  }
+
+  async checkArduinoCliAvailability(): Promise<MethodsResult> {
+    const [flag, configFilePath] = this.arduinoCliBaseParameters
+
+    const executeCommand = promisify(exec)
+    // INFO: We use the version command to check if the arduino-cli is available.
+    // INFO: If the command is not available, it will throw an error.
+    const { stdout, stderr } = await executeCommand(
+      `"${this.arduinoCliBinaryPath}" version ${flag} "${configFilePath}"`,
+    )
+    if (stderr) {
+      return { success: false, data: stderr }
+    }
+
+    return { success: true, data: stdout }
+  }
+
+  async checkIec2cAvailability(): Promise<MethodsResult> {
+    const executeCommand = promisify(exec)
+    // INFO: We use the version command to check if the iec2c is available.
+    // INFO: If the command is not available, it will throw an error.
+    const { stdout, stderr } = await executeCommand(`"${this.iec2cBinaryPath}" -v`)
+    if (stderr) {
+      return { success: false, data: stderr }
+    }
+
+    return { success: true, data: stdout }
+  }
+
+  // ++ ========================= Build Steps ================================= ++
+
+  // +++++++++++++++++++++++++ Initialization Methods ++++++++++++++++++++++++++++
+  async createBasicDirectories(projectFolderPath: string, boardTarget: string): Promise<MethodsResult> {
+    // INFO: We don't need to check if the directories already exist, as mkdir with { recursive: true } will handle that.
+    // INFO: We will create a build directory (if it does not exist), a board-specific directory, and a source directory within the board directory.
     console.log('Creating directories for project:', projectFolderPath, 'and board target:', boardTarget)
-    let res: BuildStepResult = { success: false }
+    let result: MethodsResult = { success: false }
     const buildDirectory = join(projectFolderPath, 'build')
     const boardDirectory = join(buildDirectory, boardTarget)
     const sourceDirectory = join(boardDirectory, 'src')
+
     try {
+      // Create the directories recursively.
+      // INFO: We don't have to create the build directory separately
       const results = await Promise.all([
         mkdir(boardDirectory, { recursive: true }),
         mkdir(sourceDirectory, { recursive: true }),
       ])
-      res = { success: true, data: results }
+      result = { success: true, data: results }
     } catch (_error) {
       console.error('Error creating directories')
-      res.data = _error
+      result.data = _error
     }
-    return res
+
+    return result
   }
 
-  async copyStaticBuildFiles(compilationPath: string): Promise<BuildStepResult> {
+  async copyStaticFiles(compilationPath: string): Promise<MethodsResult> {
     console.log('Copying static build files...')
-    let res: BuildStepResult = { success: false }
+    let result: MethodsResult = { success: false }
     const staticArduinoFilesPath = join(this.sourceDirectoryPath, 'arduino')
     const staticBaremetalFilesPath = join(this.sourceDirectoryPath, 'baremetal')
+
     try {
-      // Implement the logic to copy static build files
+      // Implement the logic to copy static build files.
       const results = await Promise.all([
         cp(staticArduinoFilesPath, join(compilationPath, 'arduino'), { recursive: true }),
         cp(staticBaremetalFilesPath, join(compilationPath, 'baremetal'), { recursive: true }),
       ])
       console.log('Static build files copied successfully:', results)
-      res = { success: true, data: results }
+      result = { success: true, data: results }
     } catch (error) {
       console.error(`Error copying static build files: ${String(error)}`)
-      res.data = error
+      result.data = error
     }
-    return res
+
+    return result
   }
 
-  async createFiles(): Promise<void> {}
+  // +++++++++++++++++++++++++++ Compilation Methods +++++++++++++++++++++++++++++
 
-  // ========================= Compilation Methods ============================
-
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // ++ ========================= Compiler builder ============================ ++
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
   /**
    * This will be the main entry point for the compiler module.
    * It will handle all the compilation process, will orchestrate the various steps involved in compiling a program.
