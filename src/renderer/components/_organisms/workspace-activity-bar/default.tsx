29c4b5012fedcad07399a1cce52a13545179aed7
Victor Le Roy <victorlrmatos@gmail.com>
2025-08-19 14:10:23 -0300

    fix: refactor project saving logic in DefaultWorkspaceActivityBar component

--- src/renderer/components/_organisms/workspace-activity-bar/default.tsx
+++ src/renderer/components/_organisms/workspace-activity-bar/default.tsx
@@ -10,7 +10,6 @@ import {
   ZoomButton,
 } from '../../_molecules/workspace-activity-bar/default'
 import { TooltipSidebarWrapperButton } from '../../_molecules/workspace-activity-bar/tooltip-button'
-import { saveProjectRequest } from '../../_templates'
 
 type DefaultWorkspaceActivityBarProps = {
   zoom?: {
@@ -24,8 +23,8 @@ export const DefaultWorkspaceActivityBar = ({ zoom }: DefaultWorkspaceActivityBa
     deviceDefinitions,
     deviceAvailableOptions: { availableBoards },
     workspace: { editingState },
-    workspaceActions: { setEditingState },
     consoleActions: { addLog },
+    sharedWorkspaceActions: { saveProject },
   } = useOpenPLCStore()
 
   const [isCompiling, setIsCompiling] = useState(false)
@@ -68,7 +67,11 @@ export const DefaultWorkspaceActivityBar = ({ zoom }: DefaultWorkspaceActivityBa
 
   const verifyAndCompile = async () => {
     if (editingState === 'unsaved') {
-      await saveProjectRequest({ data: projectData, meta: projectMeta }, deviceDefinitions, setEditingState)
+      const res = await saveProject({ data: projectData, meta: projectMeta }, deviceDefinitions)
+      if (!res.success) {
+        return
+      }
+
       handleRequest()
     } else {
       handleRequest()
