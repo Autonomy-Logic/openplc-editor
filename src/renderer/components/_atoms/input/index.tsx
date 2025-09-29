db998eaa3fb87be307ed56cb97df6bb977812bda
mdkrieg <mdkrieg@mtu.edu>
2025-09-28 20:19:51 -0500

    turn off spell check on inputs

--- src/renderer/components/_atoms/input/index.tsx
+++ src/renderer/components/_atoms/input/index.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithRef, forwardRef } from 'react'
 type IInputProps = ComponentPropsWithRef<'input'>
 
 const InputWithRef = forwardRef<HTMLInputElement, IInputProps>((props: IInputProps, ref) => {
-  return <input {...props} ref={ref} />
+  return <input {...props} ref={ref} spellCheck={false} />
 })
 
 export { InputWithRef }
